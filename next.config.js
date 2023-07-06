54a720d5f99f9363977a145608decb200187f72f
n <reiwa@outlook.com.vn>
2023-07-07 04:14:29 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const packageJSON = require("./package.json")
+
 /**
  * @type {import('next').NextConfig}
  **/
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: "standalone",
+  env: {
+    SENTRY_RELEASE: packageJSON.version,
+  },
+}
 
 module.exports = nextConfig
