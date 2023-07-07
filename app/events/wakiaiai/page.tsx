1dc2a70d94653742d41c0235440f4a718386e210
n <reiwa@outlook.com.vn>
2023-07-08 06:32:09 +0900

    update

--- app/events/wakiaiai/page.tsx
+++ app/events/wakiaiai/page.tsx
@@ -5,7 +5,17 @@ import { BoxSectionAboutWakiaiai } from "app/events/wakiaiai/components/BoxSecti
 export const revalidate = 60
 
 export const metadata: Metadata = {
-  title: "和気あいAI",
+  title: "和気あいAI - 展示即売会",
+  description:
+    "東海地方で初かもしれない、生成AIを利用したイラストの展示やグッズ等の展示即売会",
+  openGraph: {
+    title: "和気あいAI - 展示即売会",
+    description:
+      "2023年9月30日（土）東海地方で初かもしれない、生成AIを利用したイラストの展示やグッズ等の展示即売会！",
+    images: {
+      url: "https://www.aipictors.com/wp-content/uploads/2023/07/2NSLUKmgXQni6HaM18FAVTbtd4xscq.webp",
+    },
+  },
 }
 
 const EventWakiaiaiPage = async () => {
