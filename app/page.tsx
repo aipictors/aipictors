bf7f13ad319e4f9fc99e3f075805a7aaefbea64c
n <reiwa@outlook.com.vn>
2023-08-14 04:58:23 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { Metadata } from "next"
+import { RedirectType } from "next/dist/client/components/redirect"
+import { redirect } from "next/navigation"
+import { Config } from "config"
 
 const HomePage = async () => {
-  return null
+  redirect(Config.currentWebSiteURL, RedirectType.replace)
 }
 
 export const metadata: Metadata = {
--- config.ts
+++ config.ts
@@ -1,14 +1,6 @@
 export class Config {
-  static get firebaseConfig() {
-    return {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-    }
+  static get currentWebSiteURL() {
+    return "https://www.aipictors.com"
   }
 
   static get siteNameJA() {
@@ -41,6 +33,18 @@ export class Config {
 
   static get siteDescriptionJA() {
     return "AIイラスト・小説投稿サイト「AIピクターズ」で作品を公開してみよう！、AIイラスト・AIフォト・AIグラビア・AI小説投稿サイトです。"
+  }
+
+  static get firebaseConfig() {
+    return {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    }
   }
 
   static get graphqlEndpoint() {
