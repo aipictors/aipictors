57a140bccd797e7b74561a9fa8cc15c6dbd2a73b
fjimiz <fjtmzk0723@icloud.com>
2023-09-08 19:36:11 +0900

    update: デザインを修正する

--- app/plus/success/components/MainPlusSuccess.tsx
+++ app/plus/success/components/MainPlusSuccess.tsx
@@ -1,11 +1,45 @@
 "use client"
-import { HStack, Text } from "@chakra-ui/react"
+import { Button, HStack, Icon, Stack, Text } from "@chakra-ui/react"
+import Link from "next/link"
 import { FC } from "react"
+import { TbThumbUp } from "react-icons/tb"
 
 export const MainPlusSuccess: FC = () => {
   return (
-    <HStack justifyContent={"center"} py={16} minH={"100vh"}>
-      <Text>{"決済に成功しました。"}</Text>
-    </HStack>
+    <Stack
+      py={16}
+      minH={"100vh"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Stack spacing={16} maxW={"sm"} px={6}>
+        <Stack spacing={4}>
+          <HStack justifyContent={"center"}>
+            <Icon as={TbThumbUp} fontSize={"8rem"} color={"green.400"} />
+          </HStack>
+          <Stack>
+            <HStack
+              justifyContent={"center"}
+              fontSize={"xl"}
+              fontWeight={"bold"}
+            >
+              <Text>{"決済に成功しました。"}</Text>
+            </HStack>
+            <HStack justifyContent={"center"}>
+              <Text>
+                {
+                  "この度はピクタスにご登録ありがとうございます。これからもよろしくお願いします。"
+                }
+              </Text>
+            </HStack>
+          </Stack>
+        </Stack>
+        <HStack justifyContent={"center"}>
+          <Button as={Link} href={"/plus"} replace>
+            {"ホームに戻る"}
+          </Button>
+        </HStack>
+      </Stack>
+    </Stack>
   )
 }
