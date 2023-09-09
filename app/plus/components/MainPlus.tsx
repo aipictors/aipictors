ff05b3046f67a8990fe2919ff6baec14455d02a6
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2023-09-09 20:52:28 +0900

    規約文言追加

--- app/plus/components/MainPlus.tsx
+++ app/plus/components/MainPlus.tsx
@@ -113,7 +113,7 @@ export const MainPlus: FC = () => {
             </ListItem>
             <ListItem>
               {
-                "本プランに関し、規約違反に反する行為又は該当する恐れのある行為が確認された場合は特典を停止することがあります。詳細は当サービスの規約（<a href="https://www.aipictors.com/terms">https://www.aipictors.com/terms</a>）をご確認下さい。"
+                "本プランに関し、規約違反に反する行為又は該当する恐れのある行為が確認された場合は特典を停止することがあります。AIイラスト生成で規約違反が発覚した場合はAIイラスト生成機能の停止となる場合がございます。詳細は当サービスの規約（<a href="https://www.aipictors.com/terms">https://www.aipictors.com/terms</a>）をご確認下さい。"
               }
             </ListItem>
           </OrderedList>
