f36577c0a1b42923e0fba4ef7631bc220b9b5212
n <reiwa@outlook.com.vn>
2023-09-11 01:03:49 +0900

    update: 利用規約のリンクを追加する

--- app/components/FooterHome.tsx
+++ app/components/FooterHome.tsx
@@ -1,13 +1,36 @@
 "use client"
-import { HStack, Link as ChakraLink } from "@chakra-ui/react"
+import { HStack, Link as ChakraLink, Stack } from "@chakra-ui/react"
 import { FC } from "react"
 
 export const FooterHome: FC = () => {
   return (
-    <HStack px={4} py={4}>
-      <ChakraLink href={"https://www.aipictors.com"} fontWeight={"bold"}>
-        {"© 2023 Aipictors.com"}
-      </ChakraLink>
-    </HStack>
+    <Stack p={4}>
+      <Stack direction={{ base: "column", md: "row" }}>
+        <HStack spacing={4}>
+          <ChakraLink isExternal href={"https://www.aipictors.com/terms/"}>
+            {"利用規約"}
+          </ChakraLink>
+          <ChakraLink isExternal href={"https://www.aipictors.com/privacy/"}>
+            {"プライバシーポリシー"}
+          </ChakraLink>
+        </HStack>
+        <HStack spacing={4}>
+          <ChakraLink isExternal href={"https://www.aipictors.com/company/"}>
+            {"運営会社"}
+          </ChakraLink>
+          <ChakraLink
+            isExternal
+            href={"https://www.aipictors.com/commercialtransaction/"}
+          >
+            {"特定商取引法に基づく表記"}
+          </ChakraLink>
+        </HStack>
+      </Stack>
+      <HStack>
+        <ChakraLink href={"https://www.aipictors.com"} fontWeight={"bold"}>
+          {"© 2023 Aipictors.com"}
+        </ChakraLink>
+      </HStack>
+    </Stack>
   )
 }
