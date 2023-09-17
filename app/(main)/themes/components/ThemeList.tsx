22b710b879da95039208315a1075e164cd8dd2a3
fjimiz <fjtmzk0723@gmail.com>
2023-09-17 18:28:42 +0900

    update: カレンダーを追加する

--- app/(main)/themes/components/ThemeList.tsx
+++ app/(main)/themes/components/ThemeList.tsx
@@ -1,30 +1,54 @@
 "use client"
-import { SimpleGrid, Box, Text } from "@chakra-ui/react"
-import Link from "next/link"
+import { SimpleGrid } from "@chakra-ui/react"
 import { FC } from "react"
 import { DailyThemesQuery } from "__generated__/apollo"
+import { ThemeListItem } from "app/(main)/themes/components/ThemeListItem"
 
 type Props = {
+  year: number
+  month: number
   dailyThemesQuery: DailyThemesQuery
 }
 
+const createCalendar = (year: number, month: number) => {
+  const first = new Date(year, month - 1, 1).getDay()
+
+  const last = new Date(year, month, 0).getDate()
+
+  return [0, 1, 2, 3, 4, 5].map((weekIndex) => {
+    return [0, 1, 2, 3, 4, 5, 6].map((dayIndex) => {
+      const day = dayIndex + 1 + weekIndex * 7
+      return day - 1 < first || last < day - first ? null : day - first
+    })
+  })
+}
+
 export const ThemeList: FC<Props> = (props) => {
+  const calendar = createCalendar(2023, 9)
+  console.log(calendar)
+
+  const blocks = calendar.flat().map((day, index) => {
+    const theme = props.dailyThemesQuery.dailyThemes?.find((dailyTheme) => {
+      return dailyTheme.day === day
+    })
+    return {
+      id: `/${props.year}-${props.month}-${index}`,
+      day: day,
+      title: theme?.title ?? null,
+    }
+  })
+  console.log(blocks)
+
   return (
-    <SimpleGrid
-      as={"ul"}
-      w={"100%"}
-      minChildWidth={{ base: "180px", md: "240px" }}
-      spacing={2}
-      pr={4}
-    >
-      {props.dailyThemesQuery.dailyThemes?.map((dailyTheme) => (
-        <Box as={"li"} key={dailyTheme.id}>
-          <Link
-            href={`/themes/${dailyTheme.year}/${dailyTheme.month}/${dailyTheme.day}`}
-          >
-            <Text>{dailyTheme.title}</Text>
-          </Link>
-        </Box>
+    <SimpleGrid as={"ul"} w={"100%"} spacing={2} pr={4} columns={7}>
+      {blocks.map((block) => (
+        <ThemeListItem
+          key={block.id}
+          year={props.year}
+          month={props.month}
+          day={block.day}
+          title={block.title}
+        />
       ))}
     </SimpleGrid>
   )
