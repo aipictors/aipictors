7ba818b862873f0b1eb8bef74233810eea251495
fjimiz <fjtmzk0723@gmail.com>
2023-10-05 14:36:58 +0900

    update: ページの修正をする

--- app/(main)/stickers/[sticker]/components/StickerArticle.tsx
+++ app/(main)/stickers/[sticker]/components/StickerArticle.tsx
@@ -2,8 +2,10 @@
 import { Text, Stack } from "@chakra-ui/react"
 import React from "react"
 
-export const StickerArticle: React.FC = () => (
-  <Stack>
-    <Text>{"スタンプ詳細"}</Text>
-  </Stack>
-)
+export const StickerArticle: React.FC = () => {
+  return (
+    <Stack>
+      <Text>{"スタンプ詳細"}</Text>
+    </Stack>
+  )
+}
