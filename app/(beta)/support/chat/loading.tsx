c51d128be2fdd16b1abf47d1cca1c268d8893dae
fjimiz <fjtmzk0723@gmail.com>
2023-10-12 01:23:05 +0900

    update: ページの修正をする

---
+++ app/(beta)/support/chat/loading.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { LoadingPage } from "app/components/LoadingPage"
+
+const MessageChatLoading: React.FC = () => {
+  return <LoadingPage />
+}
+
+export default MessageChatLoading
