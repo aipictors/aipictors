d120bd850f95069ee39853d9de1e64b78013a8fa
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2023-11-08 13:33:36 +0900

    エラー修正

--- app/[lang]/(main)/_components/HomeTagList.tsx
+++ app/[lang]/(main)/_components/HomeTagList.tsx
@@ -5,7 +5,7 @@ import type { HotTagsQuery } from "__generated__/apollo"
 import { TagButton } from "app/[lang]/(main)/_components/TagButton"
 
 type Props = {
-  hotTags: HotTagsQuery["hotTags"]
+  hotTags: HotTagsQuery
 }
 
 /**
@@ -17,7 +17,7 @@ export const HomeTagList: React.FC<Props> = (props) => {
   return (
     <Box w={"100%"} overflowX={"auto"} pb={2}>
       <HStack as={"ul"} w={"100%"} spacing={2}>
-        {props.hotTags.map((tag) => (
+        {props.hotTags.hotTags?.map((tag) => (
           <TagButton key={tag.id} id={tag.id} name={tag.name} />
         ))}
         <Box p={"4px"} />
