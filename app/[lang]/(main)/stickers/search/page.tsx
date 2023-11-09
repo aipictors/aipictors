36d7fb4100aac67a20f525b0734d79549d963ef3
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2023-11-09 17:25:52 +0900

    スタンプ検索画面に新着を表示

--- app/[lang]/(main)/stickers/search/page.tsx
+++ app/[lang]/(main)/stickers/search/page.tsx
@@ -1,5 +1,6 @@
 import { Stack, Text } from "@chakra-ui/react"
-
+import { StickersDocument, StickersQuery } from "__generated__/apollo"
+import { StickerList } from "app/[lang]/(main)/stickers/_components/StickerList"
 import StickerSearchForm from "app/[lang]/(main)/stickers/_components/StickerSearchForm"
 import { createClient } from "app/_contexts/client"
 import type { Metadata } from "next"
@@ -11,10 +12,19 @@ import type { Metadata } from "next"
 const StickersSearchPage = async () => {
   const client = createClient()
 
+  const stickersQuery = await client.query<StickersQuery>({
+    query: StickersDocument,
+    variables: {
+      offset: 0,
+      limit: 64,
+    },
+  })
+
   return (
     <Stack>
       <Text>{"キーワードでスタンプを検索できます"}</Text>
       <StickerSearchForm />
+      <StickerList stickers={stickersQuery.data.stickers} />
     </Stack>
   )
 }
