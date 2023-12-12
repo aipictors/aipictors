58ed9766c4fd47ed6bd9398eee3f7658053a768b
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2023-12-12 13:37:39 +0900

    disable ppr

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   experimental: {
-    ppr: true,
+    // ppr: true, //Can only be enabled in canary
     scrollRestoration: true,
   },
   output: "standalone",
