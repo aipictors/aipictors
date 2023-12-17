0322350e88d5c60f98d72d10bbbc8ee9c1b4a281
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2023-12-17 21:40:50 +0900

    delete pnpm

--- next.config.js
+++ next.config.js
@@ -3,25 +3,28 @@ const packageJSON = require("./package.json")
 /**
  * @type {import('next').NextConfig}
  **/
-const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    // ppr: true, //Can only be enabled in canary
-    scrollRestoration: true,
-  },
-  output: "standalone",
-  env: {
-    NEXT_PUBLIC_SENTRY_RELEASE: packageJSON.version,
-  },
-}
 
-const withBundleAnalyzer = process.env.ANALYZE === 'true'
-    ? require('@next/bundle-analyzer')({ enabled: true })
-    : (config) => config;
-
-module.exports = withBundleAnalyzer({})
-module.exports = nextConfig
+module.exports = () => {
+  const nextConfig = {
+    reactStrictMode: false,
+    swcMinify: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    experimental: {
+      // ppr: true, //Can only be enabled in canary
+      scrollRestoration: true,
+    },
+    output: "standalone",
+    env: {
+      NEXT_PUBLIC_SENTRY_RELEASE: packageJSON.version,
+    },
+  };
+  if (process.env.ANALYZE === 'true') {
+    const withBundleAnalyzer = require('@next/bundle-analyzer')({
+      enabled: true,
+    });
+    return withBundleAnalyzer(nextConfig);
+  }
+  return nextConfig;
+};
