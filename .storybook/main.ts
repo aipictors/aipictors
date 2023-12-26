3be067cb5bfe125f534a1b6ae6d612154450e478
reiwa <reiwa@outlook.com.vn>
2023-12-26 20:50:37 +0900

    update: Storybookを追加する

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/nextjs"
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {
+      builder: {
+        useSWC: true, // Enables SWC support
+      },
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+}
+export default config
