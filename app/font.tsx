c10b1898f7df2130fc2ba191f66a1993b96bd40e
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-01-10 10:52:06 +0900

    switch_localfont

---
+++ app/font.tsx
@@ -0,0 +1,7 @@
+import localFont from "next/font/local"
+
+export const notoSansJp = localFont({
+  src: "./font/NotoSansJP-VariableFont_wght.ttf",
+  variable: "--font-sans",
+  display: "swap",
+})
