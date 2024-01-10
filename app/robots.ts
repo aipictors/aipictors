4429f3e03a3b217757ae872aa707f358946988d7
reiwa <reiwa@outlook.com.vn>
2024-01-10 21:45:42 +0900

    update

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next"
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/api/",
+    },
+    sitemap: "https://beta.aipictors.io/sitemap.xml",
+  }
+}
