b2ff02f1b28bbbcd61fa79dc8c17841bf3e883d2
reiwa <reiwa@outlook.com.vn>
2024-01-13 18:27:05 +0900

    update: スクロールの不具合を修正する

--- app/[lang]/(main)/_components/home-header.tsx
+++ app/[lang]/(main)/_components/home-header.tsx
@@ -2,12 +2,12 @@
 
 import { HomeNavigationList } from "@/app/[lang]/(main)/_components/home-navigation-list"
 import { HomeUserNavigationMenu } from "@/app/[lang]/(main)/_components/home-user-navigation-menu"
+import { AppHeader } from "@/components/app/app-header"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-import { useToast } from "@/components/ui/use-toast"
 import { Config } from "@/config"
-import { Bell, Folder, Menu, Search } from "lucide-react"
+import { BellIcon, FolderIcon, MenuIcon, SearchIcon } from "lucide-react"
 import Link from "next/link"
 import { useEffect } from "react"
 import { toast } from "sonner"
@@ -25,91 +25,89 @@ export const HomeHeader = (props: Props) => {
   })
 
   return (
-    <>
-      <header className="fixed z-50 w-full bg-card ">
-        <div className="flex py-4 pl-2 md:pl-8 pr-4 md:pr-8 space-x-4 border-none rounded-none shadow-none justify-between">
-          <div className="flex md:flex-1 min-w-fit items-center">
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button className="md:hidden" variant={"ghost"} size={"icon"}>
-                  <Menu />
-                </Button>
-              </SheetTrigger>
-              <SheetContent side={"left"}>
-                <HomeNavigationList
-                  onLogin={props.onLogin}
-                  onLogout={props.onLogout}
-                />
-              </SheetContent>
-            </Sheet>
-            <Link
-              className="flex items-center"
-              href="https://www.aipictors.com"
-            >
-              <img
-                src="/icon.png"
-                className="w-10 h-10 rounded-full"
-                alt="Avatar"
-              />
-            </Link>
-            <div className="pl-4 flex-1 w-full hidden md:block">
-              <Input placeholder={"作品を検索"} />
-            </div>
+    <AppHeader>
+      <div className="flex md:flex-1 min-w-fit items-center">
+        <Sheet>
+          <SheetTrigger asChild>
             <Button
-              className="md:hidden"
-              variant={"ghost"}
-              size={"icon"}
-              aria-label={"Search"}
-            >
-              <Search className="w-6" />
-            </Button>
-          </div>
-          <div className="flex space-x-2 items-center">
-            {Config.isReleaseMode ? (
-              <Button variant={"secondary"} disabled>
-                {"生成"}
-              </Button>
-            ) : (
-              <Link href={"/generation"}>
-                <Button variant={"secondary"}>{"生成"}</Button>
-              </Link>
-            )}
-            {Config.isReleaseMode ? (
-              <Button variant={"secondary"} disabled>
-                {"投稿"}
-              </Button>
-            ) : (
-              <Link href={"/new/image"}>
-                <Button variant={"secondary"}>{"投稿"}</Button>
-              </Link>
-            )}
-            {Config.isReleaseMode ? (
-              <Button variant={"secondary"} disabled size={"icon"}>
-                <Folder className="w-6" />
-              </Button>
-            ) : (
-              <Link href={"/viewer/albums"}>
-                <Button variant={"secondary"} size={"icon"}>
-                  <Folder className="w-6" />
-                </Button>
-              </Link>
-            )}
-            <Button
+              className="md:hidden mr-2"
               variant={"secondary"}
-              disabled
               size={"icon"}
-              aria-label={"通知"}
             >
-              <Bell className="w-6" />
+              <MenuIcon />
             </Button>
-            <HomeUserNavigationMenu
+          </SheetTrigger>
+          <SheetContent className="p-0" side={"left"}>
+            <HomeNavigationList
               onLogin={props.onLogin}
               onLogout={props.onLogout}
             />
-          </div>
+          </SheetContent>
+        </Sheet>
+        <Link className="flex items-center" href="https://www.aipictors.com">
+          <img
+            src="/icon.png"
+            className="w-10 h-10 rounded-full"
+            alt="Avatar"
+          />
+        </Link>
+        <div className="pl-4 flex-1 w-full hidden md:block">
+          <Input placeholder={"作品を検索"} />
         </div>
-      </header>
-      <div className={"h-header"} />
-    </>
+      </div>
+      <div className="flex space-x-2 items-center">
+        <Button
+          className="md:hidden"
+          variant={"secondary"}
+          size={"icon"}
+          aria-label={"Search"}
+        >
+          <SearchIcon className="w-4" />
+        </Button>
+        {Config.isReleaseMode ? (
+          <Button variant={"secondary"} disabled>
+            {"生成"}
+          </Button>
+        ) : (
+          <Link href={"/generation"}>
+            <Button variant={"secondary"}>{"生成"}</Button>
+          </Link>
+        )}
+        {Config.isReleaseMode ? (
+          <Button variant={"secondary"} disabled>
+            {"投稿"}
+          </Button>
+        ) : (
+          <Link href={"/new/image"}>
+            <Button variant={"secondary"}>{"投稿"}</Button>
+          </Link>
+        )}
+        {Config.isReleaseMode ? (
+          <Button variant={"secondary"} disabled size={"icon"}>
+            <FolderIcon className="w-4" />
+          </Button>
+        ) : (
+          <Link href={"/viewer/albums"}>
+            <Button variant={"secondary"} size={"icon"}>
+              <FolderIcon className="w-4" />
+            </Button>
+          </Link>
+        )}
+        {Config.isDevelopmentMode && (
+          <Button
+            variant={"secondary"}
+            disabled
+            size={"icon"}
+            aria-label={"通知"}
+          >
+            <BellIcon className="w-4" />
+          </Button>
+        )}
+        <HomeUserNavigationMenu
+          onLogin={props.onLogin}
+          onLogout={props.onLogout}
+        />
+      </div>
+    </AppHeader>
   )
 }
