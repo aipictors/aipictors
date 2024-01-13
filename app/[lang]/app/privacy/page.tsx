e7851d12307e2df790c0f9edd67c66f7df263609
reiwa <reiwa@outlook.com.vn>
2024-01-14 01:08:40 +0900

    update: マークダウンのライブラリを変更する

--- app/[lang]/app/privacy/page.tsx
+++ app/[lang]/app/privacy/page.tsx
@@ -1,6 +1,6 @@
 import { join } from "path"
 import { AppFooter } from "@/app/[lang]/app/_components/app-footer"
-import { MarkdownDocument } from "@/app/_components/markdown-document"
+import { AppMarkdown } from "@/components/app/app-markdown"
 import { AppPageCenter } from "@/components/app/app-page-center"
 import { readFile } from "fs/promises"
 import type { Metadata } from "next"
@@ -16,7 +16,7 @@ const AppPrivacyPage = async () => {
       <AppPageCenter>
         <div className="py-8 space-y-8">
           <h1 className="text-2xl font-bold">{"プライバシー・ポリシー"}</h1>
-          <MarkdownDocument>{text}</MarkdownDocument>
+          <AppMarkdown>{text}</AppMarkdown>
         </div>
       </AppPageCenter>
       <AppFooter />
--- app/[lang]/app/terms/page.tsx
+++ app/[lang]/app/terms/page.tsx
@@ -1,7 +1,6 @@
 import { join } from "path"
 import { AppFooter } from "@/app/[lang]/app/_components/app-footer"
-
-import { MarkdownDocument } from "@/app/_components/markdown-document"
+import { AppMarkdown } from "@/components/app/app-markdown"
 import { AppPageCenter } from "@/components/app/app-page-center"
 import { readFile } from "fs/promises"
 import type { Metadata } from "next"
@@ -16,8 +15,8 @@ const AppTermsPage = async () => {
     <>
       <AppPageCenter>
         <div className="py-8 space-y-8">
-          <h1 className="text-2xl font-bold"> {"利用規約"}</h1>
-          <MarkdownDocument>{text}</MarkdownDocument>
+          <h1 className="text-2xl font-bold">{"利用規約"}</h1>
+          <AppMarkdown>{text}</AppMarkdown>
         </div>
       </AppPageCenter>
       <AppFooter />
