efc807476a859f6a4978fd8a1ca199dde9290fec
L4Ph <4ranci0ne@gmail.com>
2024-01-19 12:46:50 +0900

    ロード画面を変更する (#111)
    * add suspence
    
    * 微調整

--- app/[lang]/(main)/loading.tsx
+++ app/[lang]/(main)/loading.tsx
@@ -1,5 +1,5 @@
 import AppLoadingIcon from "@/components/app/app-loading-icon"
 
-export default function RootLoading() {
+export default function MainPageLoading() {
   return <AppLoadingIcon />
 }
--- components/app/app-ripple-icon.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {
-  className?: string
-}
-
-export const AppRippleIcon = (props: Props) => {
-  return <div className="animate-ping h-4 w-4 bg-gray-600 rounded-full" />
-}
