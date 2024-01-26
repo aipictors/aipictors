5f2b93208778a641ef619d1e21bd14ea40448199
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-01-26 15:15:21 +0900

    Update generation-size.ts

--- app/[lang]/(beta)/generation/results/[result]/_types/generation-size.ts
+++ app/[lang]/(beta)/generation/results/[result]/_types/generation-size.ts
@@ -1,15 +1,13 @@
-import { ImageGenerationSizeType } from "@/__generated__/apollo"
-
 export type GenerationSize = {
   width: number
   height: number
 }
 
 export const parseGenerationSize = (
-  size: ImageGenerationSizeType,
+  size: string, // 例："SD1_512_512"
 ): GenerationSize => {
   const parts = size.split("_")
   const width = parseInt(parts[1], 10)
   const height = parseInt(parts[2], 10)
-  return { width, height } // オブジェクトとして返す
+  return { width, height }
 }
