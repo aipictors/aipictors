0e9b76524f0bdbf3c1dcf4066a09cbd1d4608a1b
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-01-28 20:07:58 +0900

    konvaでペイント機能を追加

--- app/[lang]/(beta)/generation/_components/in-painting-edit-image.tsx
+++ app/[lang]/(beta)/generation/_components/in-painting-edit-image.tsx
@@ -1,43 +1,48 @@
 import { Slider } from "@/components/ui/slider"
-// import { KonvaEventObject } from "konva/lib/Node"
-// import { useState } from "react"
-// import { Layer, tage } from "react-konva"
-// import useImage from "use-image"
+import { KonvaEventObject } from "konva/lib/Node"
+import { useState } from "react"
+import { Image, Layer, Line, Stage } from "react-konva"
+import useImage from "use-image"
 
 type Props = {
   imageUrl: string
 }
 
+type LineObject = {
+  tool: string
+  points: number[]
+}
+
 export const InPaintingEditImage = ({ imageUrl }: Props) => {
-  // const [image] = useImage(imageUrl)
-  // const [lines, setLines] = useState<LineObject[]>([])
-  // const [brushSize, setBrushSize] = useState(10)
+  const [image] = useImage(imageUrl)
+  const [lines, setLines] = useState<LineObject[]>([])
+  const [brushSize, setBrushSize] = useState(10)
 
-  // const handleMouseDown = (e: KonvaEventObject<MouseEvent>) => {
-  //   if (!e || !e.target) return
-  //   const stage = e.target.getStage()
-  //   if (!stage) return
-  //   const pos = e.target.getStage()?.getPointerPosition()
-  //   if (pos) {
-  //     setLines([...lines, { tool: "pen", points: [pos.x, pos.y] }])
-  //   }
-  // }
+  const handleMouseDown = (e: KonvaEventObject<MouseEvent>) => {
+    if (!e || !e.target) return
+    const stage = e.target.getStage()
+    if (!stage) return
+    const pos = e.target.getStage()?.getPointerPosition()
+    if (pos) {
+      setLines([...lines, { tool: "pen", points: [pos.x, pos.y] }])
+    }
+  }
 
-  // const handleMouseMove = (e: KonvaEventObject<MouseEvent>) => {
-  //   if (!lines.length) return
-  //   const stage = e.target.getStage()
-  //   const point = stage?.getPointerPosition()
-  //   const lastLine = lines[lines.length - 1]
-  //   if (point) {
-  //     lastLine.points = lastLine.points.concat([point.x, point.y])
-  //   }
-  //   lines.splice(lines.length - 1, 1, lastLine)
-  //   setLines(lines.concat())
-  // }
+  const handleMouseMove = (e: KonvaEventObject<MouseEvent>) => {
+    if (!lines.length) return
+    const stage = e.target.getStage()
+    const point = stage?.getPointerPosition()
+    const lastLine = lines[lines.length - 1]
+    if (point) {
+      lastLine.points = lastLine.points.concat([point.x, point.y])
+    }
+    lines.splice(lines.length - 1, 1, lastLine)
+    setLines(lines.concat())
+  }
 
-  // const handleMouseUp = () => {
-  //   // 線の描画終了
-  // }
+  const handleMouseUp = () => {
+    // 線の描画終了
+  }
 
   return (
     <div>
@@ -49,11 +54,37 @@ export const InPaintingEditImage = ({ imageUrl }: Props) => {
           min={1}
           max={100}
           step={0.1}
-          value={[10]}
+          value={[brushSize]}
+          onValueChange={(value) => setBrushSize(value[0])}
         />
       </div>
       <div className="flex justify-center">
-        <div className="w-full max-h-64 max-w-96 overflow-auto h-auto m-auto"></div>
+        <div className="w-full max-h-64 max-w-96 overflow-auto h-auto m-auto">
+          <Stage
+            width={window.innerWidth}
+            height={window.innerHeight}
+            onMouseDown={handleMouseDown}
+            onMousemove={handleMouseMove}
+            onMouseup={handleMouseUp}
+          >
+            <Layer>
+              <Image image={image} />
+              {lines.map((line, i) => (
+                <Line
+                  key={i}
+                  points={line.points}
+                  stroke="black"
+                  strokeWidth={brushSize}
+                  tension={0.5}
+                  lineCap="round"
+                  globalCompositeOperation={
+                    line.tool === "eraser" ? "destination-out" : "source-over"
+                  }
+                />
+              ))}
+            </Layer>
+          </Stage>
+        </div>
       </div>
     </div>
   )
