46d0c57780425b5f328e0a75b67aab1e26c80c56
reiwa <reiwa@outlook.com.vn>
2024-01-31 20:27:47 +0900

    refactor

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,24 +1,8 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/YFhY3hdw0jD
- */
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { AppNotFound } from "@/components/app/app-not-found"
 
 /**
  * next/navigationのnotFoundと名前が被るので、関数名をRootNotFoundにした。
- * @returns
  */
 export default function RootNotFound() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-9xl font-bold">404</h1>
-      <p className="mt-4 text-xl">
-        おっと! お探しのページは存在しないようです。
-      </p>
-      <Button className="mt-8 px-8 py-2 rounded-md">
-        <Link href="/">ホームに戻る</Link>
-      </Button>
-    </div>
-  )
+  return <AppNotFound />
 }
