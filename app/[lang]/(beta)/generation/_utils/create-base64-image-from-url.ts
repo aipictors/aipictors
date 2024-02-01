98ad2fa034004cd4937c629279b68c4815887ad5
reiwa <reiwa@outlook.com.vn>
2024-02-02 00:33:34 +0900

    update: 関数を追加する

---
+++ app/[lang]/(beta)/generation/_utils/create-base64-image-from-url.ts
@@ -0,0 +1,20 @@
+export function createBase64ImageFromURL(imageURL: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.setAttribute("crossOrigin", "anonymous")
+    img.onload = () => {
+      const canvas = document.createElement("canvas")
+      canvas.width = img.width
+      canvas.height = img.height
+      const ctx = canvas.getContext("2d")
+      if (ctx) {
+        ctx.drawImage(img, 0, 0)
+        const dataURL = canvas.toDataURL("image/png")
+        resolve(dataURL)
+      } else {
+        reject("ctx is null")
+      }
+    }
+    img.src = imageURL
+  })
+}
