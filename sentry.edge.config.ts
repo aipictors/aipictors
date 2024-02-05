8173ca3b3715b589ca9944b43782c40fbf40c6d2
reiwa <reiwa@outlook.com.vn>
2024-02-06 01:44:30 +0900

    refactor

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,16 +1,7 @@
-// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
-// The config you add here will be used whenever one of the edge features is loaded.
-// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import { init } from "@sentry/nextjs"
 
-import * as Sentry from "@sentry/nextjs"
-
-Sentry.init({
-  dsn: "https://8807659038d0471fa7269051b1f2cc5f@o4505182281465856.ingest.sentry.io/4505364756234240",
-
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+init({
   debug: false,
+  dsn: "https://8807659038d0471fa7269051b1f2cc5f@o4505182281465856.ingest.sentry.io/4505364756234240",
+  tracesSampleRate: 0,
 })
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,20 +1,11 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import { init } from "@sentry/nextjs"
 
-import * as Sentry from "@sentry/nextjs"
-
-Sentry.init({
-  dsn: "https://8807659038d0471fa7269051b1f2cc5f@o4505182281465856.ingest.sentry.io/4505364756234240",
-
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.01,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+init({
   debug: false,
-
+  dsn: "https://8807659038d0471fa7269051b1f2cc5f@o4505182281465856.ingest.sentry.io/4505364756234240",
   environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT!,
   release: process.env.NEXT_PUBLIC_SENTRY_RELEASE!,
+  tracesSampleRate: 0.01,
   beforeSend(event) {
     if (process.env.NODE_ENV !== "production") return null
     return event
