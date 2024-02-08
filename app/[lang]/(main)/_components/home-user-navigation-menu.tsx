7c8a0978ddaab885823ab345ea7632eb203f5c4b
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-02-08 15:08:50 +0900

    ダークモード切替UIを追加
    fix: 修正 #150

--- app/[lang]/(main)/_components/home-user-navigation-menu.tsx
+++ app/[lang]/(main)/_components/home-user-navigation-menu.tsx
@@ -6,18 +6,26 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { config } from "@/config"
 import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
   GemIcon,
   LogOutIcon,
+  MoonIcon,
   SettingsIcon,
+  SunIcon,
   UserCircleIcon,
   UserCogIcon,
 } from "lucide-react"
+import { useTheme } from "next-themes"
 import Link from "next/link"
-import { useContext } from "react"
+import { MouseEventHandler, useContext, useState } from "react"
 
 type Props = {
   onLogout(): void
@@ -38,45 +46,121 @@ const MenuItemLink = ({ href, icon, label }: MenuItemLinkProps) => (
   </Link>
 )
 
+type MenuItemMoveProps = {
+  onClick: MouseEventHandler<HTMLDivElement>
+  icon: React.ReactNode | null
+  label: string
+  isNext: boolean
+}
+
+const MenuItemMove = ({ onClick, icon, label, isNext }: MenuItemMoveProps) => (
+  <DropdownMenuLabel className="font-normal cursor-pointer" onClick={onClick}>
+    {isNext ? (
+      <div className="flex">
+        {icon ? icon : <></>}
+        <span>{label}</span>
+        {<ChevronRightIcon className="ml-auto" />}
+      </div>
+    ) : (
+      <div className="flex">
+        {<ChevronLeftIcon />}
+        {icon ? icon : <></>}
+        <span>{label}</span>
+      </div>
+    )}
+  </DropdownMenuLabel>
+)
+
 export const HomeUserNavigationMenu = (props: Props) => {
   const authContext = useContext(AuthContext)
+  const THEME_DARK = "dark"
+  const { theme, setTheme } = useTheme()
+  const [showThemeMenu, setShowThemeMenu] = useState(false)
+
+  const handleThemeOnMenuClick = (event: { stopPropagation: () => void }) => {
+    setShowThemeMenu(true)
+    event.stopPropagation()
+  }
+
+  const handleThemeOffMenuClick = (event: { stopPropagation: () => void }) => {
+    setShowThemeMenu(false)
+    event.stopPropagation()
+  }
+
+  const getThemeIcon = () => {
+    return theme === THEME_DARK ? (
+      <MoonIcon className="w-4 inline-block mr-2" />
+    ) : (
+      <SunIcon className="w-4 inline-block mr-2" />
+    )
+  }
+
+  const initMenu = () => {
+    setTimeout(() => {
+      setShowThemeMenu(false)
+    }, 400)
+  }
 
   return (
-    <DropdownMenu>
+    <DropdownMenu onOpenChange={initMenu}>
       <DropdownMenuTrigger asChild>
         <Avatar>
           <AvatarImage src={authContext.avatarPhotoURL ?? undefined} />
           <AvatarFallback />
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <MenuItemLink
-          href={`https://www.aipictors.com/users/?id=${authContext.userId}`}
-          icon={<UserCircleIcon className="w-4 inline-block mr-2" />}
-          label="マイページ"
-        />
-        <MenuItemLink
-          href="/plus"
-          icon={<GemIcon className="w-4 inline-block mr-2" />}
-          label="Aipictors+"
-        />
-        <MenuItemLink
-          href="/account/login"
-          icon={<UserCogIcon className="w-4 inline-block mr-2" />}
-          label="アカウント"
-        />
-        {config.isDevelopmentMode && (
-          <MenuItemLink
-            href="/settings/notification"
-            icon={<SettingsIcon className="w-4 inline-block mr-2" />}
-            label="設定"
+      {showThemeMenu ? (
+        <DropdownMenuContent>
+          <MenuItemMove
+            onClick={handleThemeOffMenuClick}
+            icon={null}
+            label={"テーマ"}
+            isNext={false}
           />
-        )}
-        <DropdownMenuItem onClick={props.onLogout}>
-          <LogOutIcon className="w-4 inline-block mr-2" />
-          <p>ログアウト</p>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
+          <DropdownMenuRadioGroup
+            value={theme}
+            onValueChange={(newTheme) => setTheme(newTheme)}
+          >
+            <DropdownMenuRadioItem value="light">ライト</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="dark">ダーク</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      ) : (
+        <DropdownMenuContent>
+          <MenuItemLink
+            href={`https://www.aipictors.com/users/?id=${authContext.userId}`}
+            icon={<UserCircleIcon className="w-4 inline-block mr-2" />}
+            label="マイページ"
+          />
+          <MenuItemLink
+            href="/plus"
+            icon={<GemIcon className="w-4 inline-block mr-2" />}
+            label="Aipictors+"
+          />
+          <MenuItemLink
+            href="/account/login"
+            icon={<UserCogIcon className="w-4 inline-block mr-2" />}
+            label="アカウント"
+          />
+          {config.isDevelopmentMode && (
+            <MenuItemLink
+              href="/settings/notification"
+              icon={<SettingsIcon className="w-4 inline-block mr-2" />}
+              label="設定"
+            />
+          )}
+          <MenuItemMove
+            onClick={handleThemeOnMenuClick}
+            icon={getThemeIcon()}
+            label={"テーマ"}
+            isNext={true}
+          />
+          <DropdownMenuItem onClick={props.onLogout}>
+            <LogOutIcon className="w-4 inline-block mr-2" />
+            <p>ログアウト</p>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      )}
     </DropdownMenu>
   )
 }
