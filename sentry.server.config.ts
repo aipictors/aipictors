40d83747d86377761bda5c34f09c454381f0e9f8
reiwa <reiwa@outlook.com.vn>
2024-02-08 23:30:00 +0900

    update: 環境変数を変更する

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,7 +2,7 @@ import { init } from "@sentry/nextjs"
 
 init({
   debug: false,
-  dsn: "https://8807659038d0471fa7269051b1f2cc5f@o4505182281465856.ingest.sentry.io/4505364756234240",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN!,
   environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT!,
   release: process.env.NEXT_PUBLIC_SENTRY_RELEASE!,
   tracesSampleRate: 0.01,
