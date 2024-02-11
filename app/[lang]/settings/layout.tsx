08bfadc1fb270219fc395079b679a1876a12bf9d
reiwa <reiwa@outlook.com.vn>
2024-02-11 21:38:55 +0900

    fix

--- app/[lang]/settings/layout.tsx
+++ app/[lang]/settings/layout.tsx
@@ -1,13 +1,11 @@
 "use client"
 
 import { BetaHeader } from "@/app/[lang]/(beta)/_components/beta-header"
-import { LogoutDialogLegacy } from "@/app/[lang]/_components/logout-dialog-legacy"
 import { SettingsRouteList } from "@/app/[lang]/settings/_components/settings-route-list"
 import { AuthContext } from "@/app/_contexts/auth-context"
 import { AppAside } from "@/components/app/app-aside"
 import { AppColumnLayout } from "@/components/app/app-column-layout"
 import React, { useContext } from "react"
-import { useBoolean } from "usehooks-ts"
 
 type Props = {
   children: React.ReactNode
