04d7be8ffa9cdb570a393ef5260a340dff2be297
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-02-12 17:34:50 +0900

    生成履歴で複数選択して一括削除できるようにする
    #176

--- app/[lang]/(beta)/generation/_components/generation-editor-result.tsx
+++ app/[lang]/(beta)/generation/_components/generation-editor-result.tsx
@@ -1,23 +1,23 @@
 "use client"
 
 import { GenerationEditorCard } from "@/app/[lang]/(beta)/generation/_components/generation-editor-card"
-import { GenerationResultBody } from "@/app/[lang]/(beta)/generation/_components/generation-result-body"
 import { GenerationTaskView } from "@/app/[lang]/(beta)/generation/results/[result]/_components/generation-task-view"
 import { ErrorResultCard } from "@/app/[lang]/(beta)/generation/results/_components/error-result-card"
 import { FallbackResultCard } from "@/app/[lang]/(beta)/generation/results/_components/fallback-result-card"
 import { GenerationResultCard } from "@/app/[lang]/(beta)/generation/results/_components/generation-result-card"
+import { AppConfirmDialog } from "@/components/app/app-confirm-dialog"
 import { Button } from "@/components/ui/button"
-import { Drawer, DrawerContent } from "@/components/ui/drawer"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Separator } from "@/components/ui/separator"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { Toggle } from "@/components/ui/toggle"
 import { config } from "@/config"
 import { ViewerImageGenerationTasksQuery } from "@/graphql/__generated__/graphql"
+import { deleteImageGenerationTaskMutation } from "@/graphql/mutations/delete-image-generation-task"
 import { updateRatingImageGenerationTaskMutation } from "@/graphql/mutations/update-rating-image-generation-task"
 import { useMutation } from "@apollo/client"
 import { ErrorBoundary } from "@sentry/nextjs"
-import { ArrowDownToLineIcon, StarIcon, Trash2Icon } from "lucide-react"
+import { ArrowDownToLineIcon, Check, StarIcon, Trash2Icon } from "lucide-react"
 import Link from "next/link"
 import { Suspense, useState } from "react"
 import { toast } from "sonner"
@@ -40,6 +40,7 @@ type Props = {
 
 export const GenerationEditorResult = (props: Props) => {
   const [editMode, setEditMode] = useState("")
+  const [selectedTaskIds, setSelectedTaskIds] = useState<string[]>([])
   const [mutation] = useMutation(updateRatingImageGenerationTaskMutation)
 
   const onRestore = (taskId: string) => {
@@ -55,25 +56,12 @@ export const GenerationEditorResult = (props: Props) => {
     toast("設定を復元しました")
   }
 
-  const onChangeRating = async (taskId: string, value: number) => {
-    try {
-      await mutation({
-        variables: {
-          input: {
-            id: taskId,
-            rating: value,
-          },
-        },
-      })
-    } catch (error) {
-      if (error instanceof Error) {
-        toast(error.message)
-      }
-    }
-  }
-
   const onChangeEditMode = () => {
+    if (editMode === "edit") {
+      setSelectedTaskIds([])
+    }
     setEditMode((prev) => (prev === "" ? "edit" : ""))
+    console.log(editMode)
   }
 
   const activeTasks = props.tasks?.filter((task) => {
@@ -82,6 +70,44 @@ export const GenerationEditorResult = (props: Props) => {
   })
 
   const isDesktop = useMediaQuery(config.mediaQuery.isDesktop)
+
+  const onSelectTask = (taskId: string | null) => {
+    if (!taskId) {
+      toast("存在しない履歴です")
+      return
+    }
+    const isAlreadySelected = selectedTaskIds.includes(taskId)
+
+    if (isAlreadySelected) {
+      setSelectedTaskIds(selectedTaskIds.filter((id) => id !== taskId))
+    } else {
+      setSelectedTaskIds([...selectedTaskIds, taskId])
+    }
+  }
+
+  const onCancelSelected = () => {
+    setSelectedTaskIds([])
+  }
+
+  const [deleteTask] = useMutation(deleteImageGenerationTaskMutation)
+
+  const onTrashTasks = async () => {
+    try {
+      const promises = selectedTaskIds.map((taskId) =>
+        deleteTask({
+          variables: {
+            input: {
+              id: taskId, // props.taskId ではなく、ループ内の taskId を使用
+            },
+          },
+        }),
+      )
+      await Promise.all(promises)
+      setSelectedTaskIds([])
+    } catch (error) {
+      console.error("Error in task deletion:", error)
+    }
+  }
 
   return (
     <>
@@ -95,16 +121,38 @@ export const GenerationEditorResult = (props: Props) => {
       >
         {config.isDevelopmentMode && (
           <div className="flex px-2 pb-2 space-x-2">
-            <Toggle onChange={onChangeEditMode} variant="outline">
+            <Toggle onClick={onChangeEditMode} variant="outline">
               選択
             </Toggle>
-            <Button disabled variant={"secondary"}>
+            <Button
+              onClick={onCancelSelected}
+              disabled={selectedTaskIds.length === 0}
+              variant={"secondary"}
+            >
               {"解除"}
             </Button>
-            <Button disabled variant={"ghost"} size={"icon"}>
-              <Trash2Icon className="w-4" />
-            </Button>
-            <Button disabled variant={"ghost"} size={"icon"}>
+
+            <AppConfirmDialog
+              title={"確認"}
+              description={"本当に削除しますか？"}
+              onNext={() => {
+                onTrashTasks()
+              }}
+              onCancel={() => {}}
+            >
+              <Button
+                disabled={selectedTaskIds.length === 0}
+                variant={"ghost"}
+                size={"icon"}
+              >
+                <Trash2Icon className="w-4" />
+              </Button>
+            </AppConfirmDialog>
+            <Button
+              disabled={selectedTaskIds.length === 0}
+              variant={"ghost"}
+              size={"icon"}
+            >
               <ArrowDownToLineIcon className="w-4" />
             </Button>
             <Button disabled variant={"ghost"} size={"icon"}>
@@ -118,35 +166,54 @@ export const GenerationEditorResult = (props: Props) => {
             {activeTasks.map((task) => (
               <ErrorBoundary key={task.id} fallback={ErrorResultCard}>
                 <Suspense fallback={<FallbackResultCard />}>
-                  {!isDesktop ? (
-                    <Link href={`/generation/results/${task.nanoid}`}>
-                      <Button className="p-0 h-auto overflow-hidden border-1 rounded outline-none">
-                        <GenerationResultCard
-                          taskId={task.id}
-                          token={task.token}
-                        />
-                      </Button>
-                    </Link>
-                  ) : (
-                    <Sheet>
-                      <SheetTrigger asChild>
+                  {editMode === "edit" ? (
+                    <Button
+                      onClick={() => onSelectTask(task.nanoid)}
+                      className="p-0 h-auto overflow-hidden border-1 rounded outline-none relative"
+                    >
+                      <GenerationResultCard
+                        taskId={task.id}
+                        token={task.token}
+                      />
+                      {selectedTaskIds.includes(task.nanoid ?? "") ? (
+                        <div className="absolute bg-white rounded-full right-2 bottom-2">
+                          <Check color="black" />
+                        </div>
+                      ) : (
+                        <></>
+                      )}
+                    </Button>
+                  ) : null}
+                  {editMode !== "edit" &&
+                    (!isDesktop ? (
+                      <Link href={`/generation/results/${task.nanoid}`}>
                         <Button className="p-0 h-auto overflow-hidden border-1 rounded outline-none">
                           <GenerationResultCard
                             taskId={task.id}
                             token={task.token}
                           />
                         </Button>
-                      </SheetTrigger>
-                      <SheetContent
-                        side={"left"}
-                        className="p-0 flex flex-col gap-0"
-                      >
-                        <Suspense fallback={<FallbackResultCard />}>
-                          <GenerationTaskView taskId={task.nanoid ?? ""} />
-                        </Suspense>
-                      </SheetContent>
-                    </Sheet>
-                  )}
+                      </Link>
+                    ) : (
+                      <Sheet>
+                        <SheetTrigger asChild>
+                          <Button className="p-0 h-auto overflow-hidden border-1 rounded outline-none">
+                            <GenerationResultCard
+                              taskId={task.id}
+                              token={task.token}
+                            />
+                          </Button>
+                        </SheetTrigger>
+                        <SheetContent
+                          side={"left"}
+                          className="p-0 flex flex-col gap-0"
+                        >
+                          <Suspense fallback={<FallbackResultCard />}>
+                            <GenerationTaskView taskId={task.nanoid ?? ""} />
+                          </Suspense>
+                        </SheetContent>
+                      </Sheet>
+                    ))}
                 </Suspense>
               </ErrorBoundary>
             ))}
