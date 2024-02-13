2f3d1f3b3ead0c91bbc15d3d692f8bf767fb2293
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-02-14 03:09:51 +0900

    生成履歴の設定ペインのスクロールバーと領域の空白を調整、文字位置調整
    #190

--- app/[lang]/(beta)/generation/_components/generation-editor-card.tsx
+++ app/[lang]/(beta)/generation/_components/generation-editor-card.tsx
@@ -19,7 +19,7 @@ type Props = {
 export const GenerationEditorCard = (props: Props) => {
   return (
     <Card className="h-full flex flex-col w-full overflow-hidden">
-      <div className="flex px-2 py-2 justify-between">
+      <div className="flex px-4 py-2 justify-between">
         <div className="flex items-center">
           <span className="font-bold">{props.title}</span>
         </div>
