3a6bb0371ffdd67f5415284e9dad505c16bf1b5e
reiwa <reiwa@outlook.com.vn>
2024-02-15 02:59:30 +0900

    fix: keyの不具合を修正する

--- app/[lang]/generation/tasks/_components/generation-tasks-list.tsx
+++ app/[lang]/generation/tasks/_components/generation-tasks-list.tsx
@@ -18,11 +18,15 @@ export function GenerationTasksList() {
   })
 
   return (
-    <div className="space-y-4 pb-4 px-4">
+    <div className="space-y-4 pb-4">
       <p>{"画像生成の履歴"}</p>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-2">
         {data.viewer?.imageGenerationTasks?.map((task) => (
-          <GenerationResultCard taskId={task.id} token={task.token} />
+          <GenerationResultCard
+            key={task.id}
+            taskId={task.id}
+            token={task.token}
+          />
         ))}
       </div>
     </div>
