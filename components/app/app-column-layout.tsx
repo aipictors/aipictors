9ced9635208293a1e520773778c80e986b61a1f8
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-02-14 13:37:39 +0900

    リファクタ

--- components/app/app-column-layout.tsx
+++ components/app/app-column-layout.tsx
@@ -8,7 +8,7 @@ export const AppColumnLayout = (props: Props) => {
   return (
     <div
       className={`flex items-start ${
-        props.isMaxWidthNone ? "max-w-none" : "container w-full"
+        props.isMaxWidthNone ? "max-w-none" : "w-full"
       } space-x-0 container mx-auto gap-x-4 relative`}
     >
       {props.children}
