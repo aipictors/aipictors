9a608871adc36686aa55b2421599c62dd12eeb02
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-02-17 18:07:02 +0900

    生成履歴一覧から生成画面へのリンクを追加

--- app/[lang]/generation/tasks/_components/generation-tasks-list.tsx
+++ app/[lang]/generation/tasks/_components/generation-tasks-list.tsx
@@ -7,6 +7,7 @@ import { AppLoading } from "@/components/app/app-loading"
 import { Separator } from "@/components/ui/separator"
 import { viewerCurrentPassQuery } from "@/graphql/queries/viewer/viewer-current-pass"
 import { useSuspenseQuery } from "@apollo/client"
+import Link from "next/link"
 import { Suspense, useState } from "react"
 
 export const todayText = () => {
@@ -42,7 +43,9 @@ export function GenerationTasksList() {
 
   return (
     <div className="space-y-4 pb-4 w-full">
-      <p>{"画像生成の履歴"}</p>
+      <div className="flex items-center">
+        <Link href="/generation">画像生成に戻る</Link>
+      </div>
       <GenerationTasksOperationParts
         rating={rating}
         thumbnailSize={thumbnailSize}
