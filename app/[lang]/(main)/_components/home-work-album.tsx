f86fdf6e246f8628b7739cb1997281e24109de4a
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-02-19 02:54:40 +0900

    refactor

--- app/[lang]/(main)/_components/home-work-album.tsx
+++ app/[lang]/(main)/_components/home-work-album.tsx
@@ -1,8 +1,9 @@
+"use client"
+
+import getBase64 from "@/lib/app/utils/get-base64"
 import Image from "next/image"
 import Link from "next/link"
-import { useEffect, useState } from "react"
 import type { RenderPhotoProps } from "react-photo-album"
-import { generateBase64Blur } from "../_utils/generate-blur-image" // 適切なパスに修正してください
 
 type HomeWorkAlbumProps = RenderPhotoProps & {
   workId: string
@@ -13,12 +14,7 @@ export default function HomeWorkAlbum({
   wrapperStyle,
   workId,
 }: HomeWorkAlbumProps) {
-  const [blurDataURL, setBlurDataURL] = useState<string>("") // blurDataURLの状態を管理
-
-  useEffect(() => {
-    // photo.srcが変わるたびにボカシ効果のbase64データを生成
-    generateBase64Blur(photo.src).then(setBlurDataURL).catch(console.error)
-  }, [photo.src]) // 依存配列にphoto.srcを設定
+  const blurDataURL = getBase64({ src: photo.src })
 
   return (
     <div style={{ ...wrapperStyle, position: "relative" }}>
@@ -26,12 +22,10 @@ export default function HomeWorkAlbum({
         <Image
           fill
           src={photo.src}
-          placeholder="blur" // ボカシプレースホルダーを有効化
-          blurDataURL={blurDataURL} // 生成されたボカシ効果のbase64データ
-          {...{
-            alt: "",
-            className: "rounded",
-          }}
+          alt=""
+          placeholder={"blurDataURL" in photo ? "blur" : undefined}
+          blurDataURL=""
+          className="rounded"
         />
       </Link>
     </div>
