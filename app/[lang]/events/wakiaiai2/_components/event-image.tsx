0bea2974f06833091efa2e45fa21380dd9b9d32f
K <50yen.ageru@gmail.com>
2024-02-23 15:13:06 +0900

    和気あいAI2タイトル修正

--- app/[lang]/events/wakiaiai2/_components/event-image.tsx
+++ app/[lang]/events/wakiaiai2/_components/event-image.tsx
@@ -1,11 +1,6 @@
-import { Button } from "@/components/ui/button"
-import { MousePointerClickIcon } from "lucide-react"
-
 type Props = {
   alt: string
   imageURL: string
-  linkURL: string
-  linkTitle: string
 }
 
 export const EventImage = (props: Props) => {
@@ -16,17 +11,6 @@ export const EventImage = (props: Props) => {
         src={props.imageURL}
         className="w-full object-cover object-top rounded-t-md rounded-bl-md rounded-br-3xl"
       />
-      <a
-        className="absolute bottom-4 right-4"
-        href={props.linkURL}
-        target={"_blank"}
-        rel={"noreferrer noopener"}
-      >
-        <Button className="rounded-full">
-          <MousePointerClickIcon />
-          <span className="ml-2">{props.linkTitle}</span>
-        </Button>
-      </a>
     </div>
   )
 }
