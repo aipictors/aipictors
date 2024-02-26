c5b6c724cce7d2c2e93c738bf9fedaf87f4f0085
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-02-27 00:41:16 +0900

    i2iの画像をアップロードしてタスク生成する機能を追加
    #31

--- app/[lang]/generation/_components/editor-config-view/generation-config-i2i.tsx
+++ app/[lang]/generation/_components/editor-config-view/generation-config-i2i.tsx
@@ -1,6 +1,7 @@
 import { useGenerationContext } from "@/app/[lang]/generation/_hooks/use-generation-context"
 import { parseGenerationSize } from "@/app/[lang]/generation/tasks/[task]/_types/generation-size"
 import CropImageField from "@/app/_components/crop-image-field"
+import getImageAsBase64 from "@/app/_utils/get-image-as-base64"
 
 /**
  * i2i向け画像設定
@@ -15,8 +16,11 @@ export const GenerationConfigI2i = () => {
    * クロップ完了
    * @param croppedImage クロップした画像
    */
-  const onCrop = (croppedImage: string) => {
-    context.changeI2iImageBase64(croppedImage)
+  const onCrop = async (croppedImage: string) => {
+    console.log(croppedImage)
+    const base64 = await getImageAsBase64(croppedImage)
+    console.log(base64)
+    context.changeI2iImageBase64(base64)
   }
 
   /**
---
+++ app/_utils/get-image-as-base64.ts
@@ -0,0 +1,21 @@
+/**
+ * 画像URLからBase64文字列を取得する
+ * @param url 画像URL
+ * @returns
+ */
+export default async function getImageAsBase64(url: string): Promise<string> {
+  try {
+    const response = await fetch(url)
+    const blob = await response.blob() // レスポンスをBlobとして取得
+
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onloadend = () => resolve(reader.result as string) // 読み込み完了時にBase64文字列を返す
+      reader.onerror = reject
+      reader.readAsDataURL(blob) // BlobをDataURLに変換
+    })
+  } catch (error) {
+    console.error("エラーが発生しました:", error)
+    throw error
+  }
+}
