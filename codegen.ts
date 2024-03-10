e5797024f2dae1ff0a1d267b809eebb87497bb89
reiwa <reiwa@outlook.com.vn>
2024-03-10 19:17:58 +0900

    update: エンドポイントを変更する

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli"
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://router-6ouzjmdzha-an.a.run.app",
+  schema: "https://run.aipictors.com/graphql",
   documents: "graphql/(fragments|mutations|queries)**/*.ts",
   ignoreNoDocuments: true,
   generates: {
