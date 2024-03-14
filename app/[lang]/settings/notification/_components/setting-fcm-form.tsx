f1e08d0fc58399867fd1397f871d390d0ec6d1a6
reiwa <reiwa@outlook.com.vn>
2024-03-15 02:08:06 +0900

    update: 通知機能を追加する

---
+++ app/[lang]/settings/notification/_components/setting-fcm-form.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { config } from "@/config"
+import { getMessaging, getToken } from "firebase/messaging"
+
+export function SettingFcmForm() {
+  const onClick = async () => {
+    const token = await getToken(getMessaging(), {
+      vapidKey: config.fcm.vapidKey,
+    })
+    alert(token)
+    console.log(token)
+  }
+
+  return <Button onClick={onClick}>{"FCMトークンを取得する"}</Button>
+}
