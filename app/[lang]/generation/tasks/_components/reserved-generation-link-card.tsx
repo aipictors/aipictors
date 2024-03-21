e101ef3220dee93437ff517a27beb0ac85811a2a
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-03-22 03:09:09 +0900

    スマホの時の予約タスクのデザイン調整

--- app/[lang]/generation/tasks/_components/reserved-generation-link-card.tsx
+++ app/[lang]/generation/tasks/_components/reserved-generation-link-card.tsx
@@ -46,10 +46,10 @@ export const ReservedGenerationLinkCard = (props: Props) => {
           className="h-full w-full"
           href={`/generation/tasks/${props.taskNanoid}`}
         >
-          <div className="relative flex h-full w-full">
+          <div className="relative flex">
             <div className="m-auto flex flex-col gap-y-2 p-4">
-              <Loader2Icon className="m-auto h-6 w-6 animate-spin" />
-              <span className="ta-c m-auto mb-4 text-sm">{"reserved..."}</span>
+              <Loader2Icon className="mr-auto h-6 w-6 animate-spin" />
+              <span className="ta-c mb-4 text-sm">{"reserved..."}</span>
             </div>
           </div>
         </Link>
