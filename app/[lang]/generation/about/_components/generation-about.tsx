9050649b331103fba1b07ce188daee46e1b72c66
reiwa <reiwa@outlook.com.vn>
2024-03-23 01:37:30 +0900

    fix

--- app/[lang]/generation/about/_components/generation-about.tsx
+++ app/[lang]/generation/about/_components/generation-about.tsx
@@ -3,18 +3,16 @@
 import { GlowingGradientBorderButton } from "@/app/_components/button/glowing-gradient-border-button"
 
 export const GenerationAbout = () => {
-
   return (
     <main className="flex w-full justify-center">
       <div className="w-full max-w-[30rem] space-y-8 p-4">
         <p className="font-bold text-2xl">{"画像生成"}</p>
         <p>画像生成機能について</p>
-        <GlowingGradientBorderButton 
-        className={"w-full"}
-        onClick={()=>{}}
-        children={
-        "生成"
-        } />
+        <GlowingGradientBorderButton
+          className={"w-full"}
+          onClick={() => {}}
+          children={"生成"}
+        />
       </div>
     </main>
   )
