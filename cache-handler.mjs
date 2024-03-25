6806642286041bfc703c4d21fc09f15e4e7f21b3
reiwa <reiwa@outlook.com.vn>
2024-03-25 21:05:43 +0900

    refactor

--- cache-handler.mjs
+++ cache-handler.mjs
@@ -1,16 +1,5 @@
-import { CacheHandler } from "@neshca/cache-handler"
-
-CacheHandler.onCreation(() => {
-  const handler = {
-    async get() {},
-    async set() {},
-    async revalidateTag() {},
-    async delete() {},
-  }
-
-  return {
-    handlers: [handler],
-  }
-})
-
-export default CacheHandler
+export default class CacheHandler {
+  async get() {}
+  async set() {}
+  async revalidateTag() {}
+}
