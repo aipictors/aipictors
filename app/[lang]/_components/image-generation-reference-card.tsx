b79e86c8ec25f3fd9b0fc55e73f15bd91ecefb37
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-03-27 05:45:43 +0900

    BlendAIとのコラボページ追加

---
+++ app/[lang]/_components/image-generation-reference-card.tsx
@@ -0,0 +1,27 @@
+import { Card, CardHeader } from "@/components/ui/card"
+import Link from "next/link"
+
+type Props = {
+  prompts: string
+  children: React.ReactNode
+  title: string
+}
+
+/**
+ * 画像生成参考カード
+ * @param props
+ * @returns
+ */
+export const ImageGenerationReferenceCard = (props: Props) => {
+  return (
+    <Link href={`/generation?prompts=${encodeURIComponent(props.prompts)}`}>
+      <div className="relative m-auto max-w-64">
+        <Card className="relative z-10 bg-white">{props.children}</Card>
+        <div className="ta-c relative z-10 m-auto mb-4 text-sm">
+          {props.title}
+        </div>
+        <div className="-inset-1 absolute z-0 rounded-lg bg-gradient-to-r from-red-600 to-violet-600 opacity-25 blur transition duration-1000 dark:opacity-75 group-hover:opacity-90 group-hover:duration-200" />
+      </div>
+    </Link>
+  )
+}
---
+++ app/[lang]/events/blendai/_components/event-image.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button"
+import { MousePointerClickIcon } from "lucide-react"
+
+type Props = {
+  alt: string
+  imageURL: string
+}
+
+export const EventImage = (props: Props) => {
+  return (
+    <div className="relative">
+      <img
+        alt={props.alt}
+        src={props.imageURL}
+        className="w-full rounded-t-md rounded-br-3xl rounded-bl-md object-cover object-top"
+      />
+    </div>
+  )
+}
