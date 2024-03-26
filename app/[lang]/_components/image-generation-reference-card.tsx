824a4c7355f20beb1e3be82215f334d21f12322f
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-03-27 05:56:43 +0900

    デザイン調整

--- app/[lang]/_components/image-generation-reference-card.tsx
+++ app/[lang]/_components/image-generation-reference-card.tsx
@@ -14,13 +14,18 @@ type Props = {
  */
 export const ImageGenerationReferenceCard = (props: Props) => {
   return (
-    <Link href={`/generation?prompts=${encodeURIComponent(props.prompts)}`}>
-      <div className="relative m-auto max-w-64">
-        <Card className="relative z-10 bg-white">{props.children}</Card>
-        <div className="ta-c relative z-10 m-auto mb-4 text-sm">
-          {props.title}
+    <Link
+      className="bg-gray-100 dark:bg-gray-900"
+      href={`/generation?prompts=${encodeURIComponent(props.prompts)}`}
+    >
+      <div className="rounded-md bg-gray-100 p-4 dark:bg-gray-900">
+        <div className="relative m-auto max-w-64">
+          <Card className="relative z-10 bg-white">{props.children}</Card>
+          <div className="ta-c relative z-10 m-auto mb-4 text-sm">
+            {props.title}
+          </div>
+          <div className="-inset-1 absolute z-0 rounded-lg bg-gradient-to-r from-red-600 to-violet-600 opacity-25 blur transition duration-1000 dark:opacity-75 group-hover:opacity-90 group-hover:duration-200" />
         </div>
-        <div className="-inset-1 absolute z-0 rounded-lg bg-gradient-to-r from-red-600 to-violet-600 opacity-25 blur transition duration-1000 dark:opacity-75 group-hover:opacity-90 group-hover:duration-200" />
       </div>
     </Link>
   )
