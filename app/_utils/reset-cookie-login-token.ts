f648cbccd8d090f471dc41251e76101d4b5488ee
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-04-04 00:22:00 +0900

    ログアウト時にCookieが削除されない不具合を修正

--- app/_utils/reset-cookie-login-token.ts
+++ app/_utils/reset-cookie-login-token.ts
@@ -4,5 +4,5 @@
  */
 export const resetCookieLoginToken = () => {
   // Cookieのexpires属性を1970-01-01T00:00:00Zに設定することで削除する
-  document.cookie = "wordpress.login_token=; max-age=0"
+  document.cookie = "wordpress.login_token=; max-age=0; domain=.aipictors.com;"
 }
