0fb74ab54f6401ca6d04a1442673ac85f71e2b70
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-04-05 06:15:58 +0900

    高解像度生成の補足を追加

--- app/[lang]/generation/_components/config-view/generation-config-upscale.tsx
+++ app/[lang]/generation/_components/config-view/generation-config-upscale.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useGenerationContext } from "@/app/[lang]/generation/_hooks/use-generation-context"
+import { CrossPlatformTooltip } from "@/app/_components/cross-platform-tooltip"
 import { Checkbox } from "@/components/ui/checkbox"
 
 export const GenerationConfigUpscale = () => {
@@ -8,22 +9,27 @@ export const GenerationConfigUpscale = () => {
 
   return (
     <>
-      <div className="flex items-center space-x-2">
-        <Checkbox
-          id="upscale-size-check"
-          checked={context.config.upscaleSize === 2}
-          onCheckedChange={() => {
-            context.changeUpscaleSize(
-              context.config.upscaleSize === 2 ? null : 2,
-            )
-          }}
+      <div className="flex gap-x-2">
+        <div className="flex items-center space-x-2">
+          <Checkbox
+            id="upscale-size-check"
+            checked={context.config.upscaleSize === 2}
+            onCheckedChange={() => {
+              context.changeUpscaleSize(
+                context.config.upscaleSize === 2 ? null : 2,
+              )
+            }}
+          />
+          <label
+            htmlFor="upscale-size-check"
+            className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            高解像度で生成する（extra x2）
+          </label>
+        </div>
+        <CrossPlatformTooltip
+          text={"通常に2倍の高解像度で生成できます、2枚分の消費になります。"}
         />
-        <label
-          htmlFor="upscale-size-check"
-          className="font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-        >
-          高解像度で生成する
-        </label>
       </div>
     </>
   )
