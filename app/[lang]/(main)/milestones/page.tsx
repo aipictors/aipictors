1b6fd3fcd04ac3dce42bbaa54234c8703da7b939
N.K <124619691+nakatsuka-k@users.noreply.github.com>
2024-04-05 11:40:46 +0900

    マイルストーンページの見た目修正

--- app/[lang]/(main)/milestones/page.tsx
+++ app/[lang]/(main)/milestones/page.tsx
@@ -1,4 +1,10 @@
 import { AppPage } from "@/components/app/app-page"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 import { Card } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 import {
@@ -9,6 +15,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { milestonesQuery } from "@/graphql/queries/milestone/miestones"
 import { createClient } from "@/lib/client"
 import type { Metadata } from "next"
@@ -49,178 +56,225 @@ const MilestonesPage = async () => {
 
   return (
     <AppPage>
-      <div className="flex flex-col gap-y-4">
-        <h1 className="font-bold">{"開発予定"}</h1>
-        <div className="relative flex h-16 w-full items-center justify-center overflow-hidden rounded-lg bg-gray-800">
-          <div className="absolute top-6 left-4 h-16 w-16 rounded-lg font-bold text-sm text-white">
-            {"Web版"}
+      <h1 className="font-bold">{"開発予定"}</h1>
+      <Tabs defaultValue="web" className="w-full">
+        <TabsList className="mb-4 grid w-full grid-cols-2">
+          <TabsTrigger value="web">web</TabsTrigger>
+          <TabsTrigger value="app">app</TabsTrigger>
+        </TabsList>
+        <TabsContent value="web" className="w-full">
+          <div className="flex flex-col gap-y-4">
+            <div className="relative flex h-16 w-full items-center justify-center overflow-hidden rounded-lg bg-gray-800">
+              <div className="absolute top-6 left-4 h-16 w-16 rounded-lg font-bold text-sm text-white">
+                {"Web版"}
+              </div>
+              <img
+                className="h-full w-full object-cover"
+                alt=""
+                src="https://www.aipictors.com/wp-content/themes/AISite/images/milestone-web-back.webp"
+              />
+            </div>
+            <h2 className="font-bold text-sm">{"Web - これから"}</h2>
+            <Card>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="min-w-28">{"バージョン"}</TableHead>
+                    <TableHead className="min-w-64">{"タイトル"}</TableHead>
+                    <TableHead className="min-w-80">{"開発内容"}</TableHead>
+                    <TableHead className="min-w-16">{"完了予定"}</TableHead>
+                    <TableHead className="min-w-32">{"GitHub"}</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {milestones.map(
+                    (milestone) =>
+                      !milestone.isDone && (
+                        <TableRow key={milestone.id}>
+                          <TableCell className="font-bold">
+                            {milestone.version}
+                          </TableCell>
+                          <TableCell>{milestone.title}</TableCell>
+                          <TableCell>{milestone.description}</TableCell>
+                          <TableCell>
+                            {milestone.dueDate
+                              ? convertDateFormat(milestone.dueDate)
+                              : "未定"}
+                          </TableCell>
+                          <TableCell>
+                            <Link href={milestone.pageURL} target="_blank">
+                              {"GitHubで確認"}
+                            </Link>
+                          </TableCell>
+                        </TableRow>
+                      ),
+                  )}
+                </TableBody>
+              </Table>
+            </Card>
+            <div className="py-2">
+              <Separator />
+            </div>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="setting">
+                <AccordionTrigger>
+                  <h2 className="font-bold text-sm">
+                    {"アプリ - リリース済み"}
+                  </h2>
+                </AccordionTrigger>
+                <AccordionContent className="space-y-2">
+                  <Card>
+                    <Table>
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead className="min-w-28">
+                            {"バージョン"}
+                          </TableHead>
+                          <TableHead className="min-w-64">
+                            {"タイトル"}
+                          </TableHead>
+                          <TableHead className="min-w-80">
+                            {"開発内容"}
+                          </TableHead>
+                          <TableHead className="min-w-32">{"GitHub"}</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        {milestones.map(
+                          (milestone) =>
+                            milestone.isDone && (
+                              <TableRow key={milestone.id}>
+                                <TableCell className="font-bold">
+                                  {milestone.version}
+                                </TableCell>
+                                <TableCell>{milestone.title}</TableCell>
+                                <TableCell>{milestone.description}</TableCell>
+                                <TableCell>
+                                  <Link
+                                    href={milestone.pageURL}
+                                    target="_blank"
+                                  >
+                                    {"GitHubで確認"}
+                                  </Link>
+                                </TableCell>
+                              </TableRow>
+                            ),
+                        )}
+                      </TableBody>
+                    </Table>
+                  </Card>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
-          <img
-            className="h-full w-full object-cover"
-            alt=""
-            src="https://www.aipictors.com/wp-content/themes/AISite/images/milestone-web-back.webp"
-          />
-        </div>
-        <h2 className="font-bold text-sm">{"Web - これから"}</h2>
-        <Card>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="min-w-28">{"バージョン"}</TableHead>
-                <TableHead className="min-w-64">{"タイトル"}</TableHead>
-                <TableHead className="min-w-80">{"開発内容"}</TableHead>
-                <TableHead className="min-w-16">{"完了予定"}</TableHead>
-                <TableHead className="min-w-32">{"GitHub"}</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {milestones.map(
-                (milestone) =>
-                  !milestone.isDone && (
-                    <TableRow key={milestone.id}>
-                      <TableCell className="font-bold">
-                        {milestone.version}
-                      </TableCell>
-                      <TableCell>{milestone.title}</TableCell>
-                      <TableCell>{milestone.description}</TableCell>
-                      <TableCell>
-                        {milestone.dueDate
-                          ? convertDateFormat(milestone.dueDate)
-                          : "未定"}
-                      </TableCell>
-                      <TableCell>
-                        <Link href={milestone.pageURL} target="_blank">
-                          {"GitHubで確認"}
-                        </Link>
-                      </TableCell>
-                    </TableRow>
-                  ),
-              )}
-            </TableBody>
-          </Table>
-        </Card>
-        <div className="py-2">
-          <Separator />
-        </div>
-        <h2 className="font-bold text-sm">{"Web - リリース済み"}</h2>
-        <Card>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="min-w-28">{"バージョン"}</TableHead>
-                <TableHead className="min-w-64">{"タイトル"}</TableHead>
-                <TableHead className="min-w-80">{"開発内容"}</TableHead>
-                <TableHead className="min-w-32">{"GitHub"}</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {milestones.map(
-                (milestone) =>
-                  milestone.isDone && (
-                    <TableRow key={milestone.id}>
-                      <TableCell className="font-bold">
-                        {milestone.version}
-                      </TableCell>
-                      <TableCell>{milestone.title}</TableCell>
-                      <TableCell>{milestone.description}</TableCell>
-                      <TableCell>
-                        <Link href={milestone.pageURL} target="_blank">
-                          {"GitHubで確認"}
-                        </Link>
-                      </TableCell>
-                    </TableRow>
-                  ),
-              )}
-            </TableBody>
-          </Table>
-        </Card>
-        <div className="py-2">
-          <Separator />
-        </div>
-        <div className="relative flex h-16 w-full items-center justify-center overflow-hidden rounded-lg bg-gray-800">
-          <div className="absolute top-6 left-4 h-16 w-16 rounded-lg font-bold text-sm text-white">
-            {"アプリ版"}
+        </TabsContent>
+        <TabsContent value="app" className="w-full">
+          <div className="flex flex-col gap-y-4">
+            <div className="relative flex h-16 w-full items-center justify-center overflow-hidden rounded-lg bg-gray-800">
+              <div className="absolute top-6 left-4 h-16 w-16 rounded-lg font-bold text-sm text-white">
+                {"アプリ版"}
+              </div>
+              <img
+                className="h-full w-full object-cover"
+                alt=""
+                src="https://www.aipictors.com/wp-content/themes/AISite/images/milestone-app-back.webp"
+              />
+            </div>
+            <h2 className="font-bold text-sm">{"App - これから"}</h2>
+            <Card>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="min-w-28">{"バージョン"}</TableHead>
+                    <TableHead className="min-w-64">{"タイトル"}</TableHead>
+                    <TableHead className="min-w-80">{"開発内容"}</TableHead>
+                    <TableHead className="min-w-16">{"完了予定"}</TableHead>
+                    <TableHead className="min-w-32">{"GitHub"}</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {appMilestones.map(
+                    (milestone) =>
+                      !milestone.isDone && (
+                        <TableRow key={milestone.id}>
+                          <TableCell className="font-bold">
+                            {milestone.version}
+                          </TableCell>
+                          <TableCell>{milestone.title}</TableCell>
+                          <TableCell>{milestone.description}</TableCell>
+                          <TableCell>
+                            {milestone.dueDate
+                              ? convertDateFormat(milestone.dueDate)
+                              : "未定"}
+                          </TableCell>
+                          <TableCell>
+                            <Link href={milestone.pageURL} target="_blank">
+                              {"GitHubで確認"}
+                            </Link>
+                          </TableCell>
+                        </TableRow>
+                      ),
+                  )}
+                </TableBody>
+              </Table>
+            </Card>
+            <div className="py-2">
+              <Separator />
+            </div>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="setting">
+                <AccordionTrigger>
+                  <h2 className="font-bold text-sm">
+                    {"アプリ - リリース済み"}
+                  </h2>
+                </AccordionTrigger>
+                <AccordionContent className="space-y-2">
+                  <Card>
+                    <Table>
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead className="min-w-28">
+                            {"バージョン"}
+                          </TableHead>
+                          <TableHead className="min-w-64">
+                            {"タイトル"}
+                          </TableHead>
+                          <TableHead className="min-w-80">
+                            {"開発内容"}
+                          </TableHead>
+                          <TableHead className="min-w-32">{"GitHub"}</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        {appMilestones.map(
+                          (milestone) =>
+                            milestone.isDone && (
+                              <TableRow key={milestone.id}>
+                                <TableCell className="font-bold">
+                                  {milestone.version}
+                                </TableCell>
+                                <TableCell>{milestone.title}</TableCell>
+                                <TableCell>{milestone.description}</TableCell>
+                                <TableCell>
+                                  <Link
+                                    href={milestone.pageURL}
+                                    target="_blank"
+                                  >
+                                    {"GitHubで確認"}
+                                  </Link>
+                                </TableCell>
+                              </TableRow>
+                            ),
+                        )}
+                      </TableBody>
+                    </Table>
+                  </Card>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
-          <img
-            className="h-full w-full object-cover"
-            alt=""
-            src="https://www.aipictors.com/wp-content/themes/AISite/images/milestone-app-back.webp"
-          />
-        </div>
-        <h2 className="font-bold text-sm">{"アプリ - これから"}</h2>
-        <Card>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="min-w-28">{"バージョン"}</TableHead>
-                <TableHead className="min-w-64">{"タイトル"}</TableHead>
-                <TableHead className="min-w-80">{"開発内容"}</TableHead>
-                <TableHead className="min-w-16">{"完了予定"}</TableHead>
-                <TableHead className="min-w-32">{"GitHub"}</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {appMilestones.map(
-                (milestone) =>
-                  !milestone.isDone && (
-                    <TableRow key={milestone.id}>
-                      <TableCell className="font-bold">
-                        {milestone.version}
-                      </TableCell>
-                      <TableCell>{milestone.title}</TableCell>
-                      <TableCell>{milestone.description}</TableCell>
-                      <TableCell>
-                        {milestone.dueDate
-                          ? convertDateFormat(milestone.dueDate)
-                          : "未定"}
-                      </TableCell>
-                      <TableCell>
-                        <Link href={milestone.pageURL} target="_blank">
-                          {"GitHubで確認"}
-                        </Link>
-                      </TableCell>
-                    </TableRow>
-                  ),
-              )}
-            </TableBody>
-          </Table>
-        </Card>
-        <div className="py-2">
-          <Separator />
-        </div>
-        <h2 className="font-bold text-sm">{"アプリ - リリース済み"}</h2>
-        <Card>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="min-w-28">{"バージョン"}</TableHead>
-                <TableHead className="min-w-64">{"タイトル"}</TableHead>
-                <TableHead className="min-w-80">{"開発内容"}</TableHead>
-                <TableHead className="min-w-32">{"GitHub"}</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {appMilestones.map(
-                (milestone) =>
-                  milestone.isDone && (
-                    <TableRow key={milestone.id}>
-                      <TableCell className="font-bold">
-                        {milestone.version}
-                      </TableCell>
-                      <TableCell>{milestone.title}</TableCell>
-                      <TableCell>{milestone.description}</TableCell>
-                      <TableCell>
-                        <Link href={milestone.pageURL} target="_blank">
-                          {"GitHubで確認"}
-                        </Link>
-                      </TableCell>
-                    </TableRow>
-                  ),
-              )}
-            </TableBody>
-          </Table>
-        </Card>
-      </div>
+        </TabsContent>
+      </Tabs>
     </AppPage>
   )
 }
