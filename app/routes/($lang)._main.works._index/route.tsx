bde95f29ea66d8877c7e239d077fdb6dbb10f247
fmzu <149039422+fmzu@users.noreply.github.com>
2024-04-10 00:19:11 +0900

    update:　ページを追加する

---
+++ app/routes/($lang)._main.works._index/route.tsx
@@ -0,0 +1,15 @@
+import WorksPageLoading from "@/[lang]/(main)/works/loading"
+import { AppPlaceholder } from "@/_components/app/app-placeholder"
+import type { Metadata } from "next"
+import { Suspense } from "react"
+
+/**
+ * 作品一覧ページ
+ */
+export default function Works() {
+  return (
+    <Suspense fallback={<WorksPageLoading />}>
+      <AppPlaceholder>{"作品の一覧"}</AppPlaceholder>
+    </Suspense>
+  )
+}
