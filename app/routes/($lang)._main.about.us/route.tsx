2b8213c312fb57f9fd8e8560c2fd6ece352395b3
fmzu <149039422+fmzu@users.noreply.github.com>
2024-04-09 22:09:52 +0900

    update: ページを追加する

---
+++ app/routes/($lang)._main.about.us/route.tsx
@@ -0,0 +1,20 @@
+import { AppPage } from "@/_components/app/app-page"
+import type { Metadata } from "next"
+
+/**
+ * 組織について
+ * @returns
+ */
+export default function AboutUs() {
+  return (
+    <AppPage>
+      <div className="flex flex-col">
+        <h2 className="py-2 font-bold text-2xl">運営会社について</h2>
+        <p>
+          Aipictors株式会社が運営しております。お問い合わせは
+          hello@aipictors.com からお願い致します。
+        </p>
+      </div>
+    </AppPage>
+  )
+}
