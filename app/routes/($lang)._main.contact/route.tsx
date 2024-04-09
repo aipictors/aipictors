dc97629246daae320ee1e7f9385bef6d05fbce56
fmzu <149039422+fmzu@users.noreply.github.com>
2024-04-09 21:56:53 +0900

    update: ページを追加する

---
+++ app/routes/($lang)._main.contact/route.tsx
@@ -0,0 +1,19 @@
+import { readFile } from "node:fs/promises"
+import { join } from "node:path"
+import { AppPageCenter } from "@/_components/app/app-page-center"
+import { Link, useLoaderData } from "@remix-run/react"
+import type { Metadata } from "next"
+
+export default function Contact() {
+  return (
+    <AppPageCenter>
+      <div className="w-full space-y-8 py-8">
+        <h1 className="font-bold text-2xl">{"お問い合わせ"}</h1>
+        <h2 className="font-bold text-md">運営への問い合わせ</h2>
+        <Link to="/support/chat">チャットで問い合わせる</Link>
+        <h2 className="font-bold text-md">法人に関するお問い合わせ</h2>
+        {"hello@aipictors.com"}
+      </div>
+    </AppPageCenter>
+  )
+}
