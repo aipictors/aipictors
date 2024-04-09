232ebca8d45c4a3234161b3bc485591d43ffb428
fmzu <149039422+fmzu@users.noreply.github.com>
2024-04-09 22:18:35 +0900

    update: ページを追加する

---
+++ app/routes/($lang)._main.guideline/route.tsx
@@ -0,0 +1,14 @@
+import { GuidelineArticle } from "@/[lang]/(main)/guideline/_components/guideline-article"
+import { AppPage } from "@/_components/app/app-page"
+
+/**
+ * ガイドライン
+ * @returns
+ */
+export default function Guideline() {
+  return (
+    <AppPage>
+      <GuidelineArticle />
+    </AppPage>
+  )
+}
