74a8a8210495b7aec6b82904021693341e157287
fmzu <149039422+fmzu@users.noreply.github.com>
2024-04-09 22:58:59 +0900

    update: ページを追加する

---
+++ app/routes/($lang)._main.collections.$collection/route.tsx
@@ -0,0 +1,38 @@
+import { CollectionArticle } from "@/[lang]/(main)/collections/[collection]/_components/collection-article"
+import { WorkList } from "@/[lang]/(main)/works/_components/work-list"
+import { AppPage } from "@/_components/app/app-page"
+import { worksQuery } from "@/_graphql/queries/work/works"
+import { createClient } from "@/_lib/client"
+import { useLoaderData } from "@remix-run/react"
+import type { Metadata } from "next"
+
+/**
+ * コレクションの詳細
+ * @returns
+ */
+export async function loader() {
+  const client = createClient()
+
+  const worksResp = await client.query({
+    query: worksQuery,
+    variables: {
+      offset: 0,
+      limit: 16,
+      where: {},
+    },
+  })
+  return {
+    worksResp,
+  }
+}
+
+export default function Collections() {
+  const data = useLoaderData<typeof loader>()
+
+  return (
+    <AppPage>
+      <CollectionArticle />
+      <WorkList works={data.worksResp.data.works ?? []} />
+    </AppPage>
+  )
+}
