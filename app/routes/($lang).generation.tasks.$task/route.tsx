88217772d0392fe10745a7c3eb5c2d7a3544beaa
fmzu <149039422+fmzu@users.noreply.github.com>
2024-04-14 01:08:41 +0900

    update: ページを修正する

--- app/routes/($lang).generation.tasks.$task/route.tsx
+++ app/routes/($lang).generation.tasks.$task/route.tsx
@@ -1,3 +1,23 @@
+import { GenerationTaskView } from "@/[lang]/generation/tasks/[task]/_components/generation-task-view"
+import { useParams } from "@remix-run/react"
+import { useLoaderData } from "@remix-run/react"
+
+export async function loader() {
+  return {}
+}
+
 export default function Task() {
-  return null
+  const params = useParams()
+
+  if (params.task === undefined) {
+    return null
+  }
+
+  const data = useLoaderData<typeof loader>()
+
+  return (
+    <div className="mx-auto w-full max-w-fit">
+      <GenerationTaskView taskId={params.task} />
+    </div>
+  )
 }
