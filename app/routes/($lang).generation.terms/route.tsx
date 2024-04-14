fe9b98dc51dc32c6a1ee76c136120e71bc492637
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-04-15 02:27:35 +0900

    Crop機能を修正、ビルドを通す

--- app/routes/($lang).generation.terms/route.tsx
+++ app/routes/($lang).generation.terms/route.tsx
@@ -1,21 +1,14 @@
-import { readFile } from "node:fs/promises"
-import { join } from "node:path"
+import termsMarkdownText from "@/_assets/flutter/privacy.md?raw"
 import { AppMarkdown } from "@/_components/app/app-markdown"
 import { AppPageCenter } from "@/_components/app/app-page-center"
 import { Button } from "@/_components/ui/button"
 import { Link } from "@remix-run/react"
-import type { Metadata } from "next"
 
 /**
  * 画像生成機能の利用規約
  * @returns
  */
 export default function GenerationTerms() {
-  const termsMarkdownText = readFile(
-    join(process.cwd(), "assets/image-generation-terms.md"),
-    "utf-8",
-  )
-
   return (
     <>
       <AppPageCenter>
