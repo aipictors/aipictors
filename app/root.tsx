d5c9f49b0ace6935e64fdac76e9469ec609a63aa
reiwa <reiwa@outlook.com.vn>
2024-04-15 02:40:07 +0900

    fix: フォントの設定の不具合を修正する

--- app/root.tsx
+++ app/root.tsx
@@ -15,22 +15,7 @@ import { Suspense } from "react"
 import { Toaster } from "sonner"
 
 export const links: LinksFunction = () => {
-  return [
-    { rel: "stylesheet", href: styles },
-    {
-      rel: "preconnect",
-      href: "https://fonts.googleapis.com",
-    },
-    {
-      href: "https://fonts.googleapis.com/css2?family=family=Noto+Sans+JP:wght@100..900&display=swap",
-      rel: "stylesheet",
-    },
-    {
-      rel: "preconnect",
-      href: "https://fonts.gstatic.com",
-      crossOrigin: "anonymous",
-    },
-  ]
+  return [{ rel: "stylesheet", href: styles }]
 }
 
 export default function Root() {
@@ -39,6 +24,16 @@ export default function Root() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={""}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
