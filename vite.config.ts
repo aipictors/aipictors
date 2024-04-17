e0cb076540f9f12f10e5c223d1216d67e9dc9faa
reiwa <reiwa@outlook.com.vn>
2024-04-18 00:47:31 +0900

    fix: 設定を元に戻す

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
+  ssr: {
+    noExternal: ["react-easy-crop", "tslib"],
+  },
   plugins: [
     // Error: Failed to execute 'insertBefore' on 'Node': The node before which the new node is to be inserted is not a child of this node.
     // million.vite({ auto: true }),
