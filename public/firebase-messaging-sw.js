c04e5796fe5ea1657d3e2aeae0e5c2e129913150
reiwa <reiwa@outlook.com.vn>
2024-04-23 00:08:36 +0900

    update: 通知の設定を変更する

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -13,13 +13,15 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-const messaging = firebase.messaging();
+firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: './icon.png',
-  }
-  self.registration.showNotification(notificationTitle, notificationOptions);
-})
+// const messaging = firebase.messaging();
+
+// messaging.onBackgroundMessage((payload) => {
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: './icon.png',
+//   }
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// })
