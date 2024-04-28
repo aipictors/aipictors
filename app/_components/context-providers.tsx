150ebe7464fffa78b7b5c3426b98c9483ed813e9
mikage <mikage@sawatari.info>
2024-04-28 10:46:21 +0900

    通知は出るようになったきがするけど多重で出る。一旦コミット

--- app/_components/context-providers.tsx
+++ app/_components/context-providers.tsx
@@ -6,7 +6,7 @@ import { ApolloProvider } from "@apollo/client/index.js"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { initializeAnalytics } from "firebase/analytics"
 import { getApp, getApps, initializeApp } from "firebase/app"
-import { getMessaging, onMessage } from "firebase/messaging"
+import { getMessaging, onMessage, getToken } from "firebase/messaging"
 type Props = {
   children: React.ReactNode
 }
@@ -33,14 +33,20 @@ if (typeof window !== "undefined" && getApps().length === 0) {
   try {
     getMessaging(getApp())
     onMessage(getMessaging(), (payload) => {
-      navigator.serviceWorker.ready.then((registration) => {
-        if (payload.data === undefined) return
-        registration.showNotification(payload.data.title, {
-          body: payload.data.body,
-          icon: payload.data.icon,
-          data: payload.data,
-          // @ts-ignore https://developer.mozilla.org/ja/docs/Web/API/ServiceWorkerRegistration/showNotification#image
-          image: payload.data.imageUrl,
+      console.log("onMessage", payload)
+      getToken(getMessaging(), {
+        vapidKey: config.fcm.vapidKey,
+      }).then((token) => {
+        navigator.serviceWorker.ready.then((registration) => {
+          if (payload.data === undefined) return
+          console.log("serviceWorker.ready", payload)
+          registration.showNotification(payload.data.title, {
+            body: payload.data.body,
+            icon: payload.data.icon,
+            data: payload.data,
+            // @ts-ignore https://developer.mozilla.org/ja/docs/Web/API/ServiceWorkerRegistration/showNotification#image
+            image: payload.data.imageUrl,
+          })
         })
       })
     })
