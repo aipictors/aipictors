3ea0af9724d7b76ca9c5919be0e70eec59d70580
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-06-09 21:50:00 +0900

    Update route.tsx

--- app/routes/($lang).sensitive/route.tsx
+++ app/routes/($lang).sensitive/route.tsx
@@ -3,6 +3,7 @@ import { AppColumnLayout } from "@/_components/app/app-column-layout"
 import { HomeFooter } from "@/_components/home-footer"
 import HomeHeader from "@/routes/($lang)._main._index/_components/home-header"
 import { HomeRouteList } from "@/routes/($lang)._main._index/_components/home-route-list"
+import { HomeRouteVerticalList } from "@/routes/($lang)._main._index/_components/home-route-vertical-list"
 import type { MetaFunction } from "@remix-run/cloudflare"
 import { Outlet } from "@remix-run/react"
 
@@ -14,10 +15,15 @@ export default function SensitiveLayout() {
   return (
     <>
       <HomeHeader />
+      <div className="hidden h-8 md:block">
+        <HomeRouteVerticalList />
+      </div>
       <AppColumnLayout>
-        <AppAside>
-          <HomeRouteList />
-        </AppAside>
+        <div className="block md:hidden">
+          <AppAside>
+            <HomeRouteList />
+          </AppAside>
+        </div>
         <Outlet />
       </AppColumnLayout>
       <HomeFooter />
