06a98ae253f3d8945ee922895ada1cee5c84df6e
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-06-11 22:36:00 +0900

    デザイン調整

--- app/routes/($lang)._main.works.$work/_components/prompton-request-colorful-button.tsx
+++ app/routes/($lang)._main.works.$work/_components/prompton-request-colorful-button.tsx
@@ -28,9 +28,9 @@ export const PromptonRequestColorfulButton = (props: Props) => {
       onClick={onClick}
       // biome-ignore lint/a11y/useButtonType: <explanation>
       // biome-ignore lint/nursery/useSortedClasses: <explanation>
-      className={`flex w-full items-center ${props.rounded} bg-gradient-to-r from-orange-400 via-pink-500 to-blue-500 px-2 py-1 text-white shadow-lg focus:outline-none`}
+      className={`flex w-full h-8 md:h-10 items-center ${props.rounded} bg-gradient-to-r from-orange-400 via-pink-500 to-blue-500 px-2 py-1 text-white shadow-lg focus:outline-none`}
     >
-      <div className="m-auto flex items-center p-1">
+      <div className="m-auto flex items-center">
         {props.hideIcon !== true && (
           <span className="rounded-full bg-white bg-opacity-30 p-1">
             <GiftIcon className="h-4 w-4" />
