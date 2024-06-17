1ec610606c38cffb1519306bd53c9f1d16459050
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-06-18 04:36:23 +0900

    Update header-develop-banner.tsx

--- app/_components/header-develop-banner.tsx
+++ app/_components/header-develop-banner.tsx
@@ -4,10 +4,10 @@ export const HeaderDevelopBanner = () => {
   return (
     <Link
       to={"https://www.aipictors.com"}
-      className="fixed top-[72px] z-50 h-8 w-full bg-zinc-950 text-center font-bold text-white"
+      className="fixed top-[72px] z-50 w-full bg-zinc-950 pt-1 pb-1 text-center font-bold text-white"
     >
       {
-        "こちらは開発中のベータ版です。現行のサイトはこちらをクリックしてください。"
+        "こちらは開発中のベータ版です。正常に動作しない可能性があります。現行のサイトはこちらをクリックしてください。"
       }
     </Link>
   )
