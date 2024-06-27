53a7594326c9b638ae4312821dec96662a5d595a
syuribox <33959988+syuribox@users.noreply.github.com>
2024-06-27 22:51:33 +0900

    マイページ小説一覧をflex-warpで再実装
    ・元々ホーム表示と共通のCarouselWithGradationで実装されていたのをflex-warpに変更

--- app/routes/($lang)._main.users.$user.novels/_components/user-novel-list.tsx
+++ app/routes/($lang)._main.users.$user.novels/_components/user-novel-list.tsx
@@ -1,3 +1,78 @@
-export const UserNovelList = () => {
-  return <div />
+import { config } from "@/config"
+import { IconUrl } from "@/_components/icon-url"
+import { LikeButton } from "@/_components/like-button"
+import { NovelWorkPreviewItem } from "@/_components/novel-work-preview-item"
+import { UserNameBadge } from "@/_components/user-name-badge"
+import { PhotoAlbum } from "react-photo-album"
+import type { worksQuery } from "@/_graphql/queries/work/works"
+import type { ResultOf } from "gql.tada"
+import { useMediaQuery } from "usehooks-ts"
+
+type Props = {
+  works: NonNullable<ResultOf<typeof worksQuery>["works"]> | null
+  targetRowHeight?: number
+}
+
+/**
+ * 小説作品一覧
+ */
+export const UserNovelList = (props: Props) => {
+  if (!props.works || props.works.length === 0) {
+    return null
+  }
+
+  const workResults = props.works.map((work) => ({
+    id: work.id,
+    src: work.smallThumbnailImageURL,
+    workId: work.id,
+    userId: work.user.id,
+    userIcon: work.user.iconUrl,
+    userName: work.user.name,
+    title: work.title,
+    isLiked: work.isLiked,
+    text: work.title,
+  }))
+
+  const works = workResults
+
+  if (works.length === 0) {
+    return null
+  }
+
+  return (
+    <section className="m-2">
+      <div className="flex-wrap space-y-4 justify-items-center">
+        {works.map((work) => (
+          <div key={work.id}
+            className="relative rounded border-2 border-gray border-solid ml-4 inline-block"
+          >
+            <NovelWorkPreviewItem
+              workId={work.id}
+              imageUrl={work.src}
+              title={work.title}
+              text={work.text ?? ""}
+              tags={[]}
+            />
+            <UserNameBadge
+              userId={work.userId}
+              userIconImageURL={IconUrl(work.userIcon)}
+              name={work.userName}
+              width={"lg"}
+            />
+            <div className="absolute right-0 bottom-0">
+              <LikeButton
+                size={56}
+                targetWorkId={work.id}
+                targetWorkOwnerUserId={work.userId}
+                defaultLiked={work.isLiked}
+                defaultLikedCount={0}
+                isBackgroundNone={true}
+                strokeWidth={2}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
