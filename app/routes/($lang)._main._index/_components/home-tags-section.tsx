f0680d291fd608d3ee7bdd20192c7835a72c3e88
syuribox <33959988+syuribox@users.noreply.github.com>
2024-06-28 10:09:05 +0900

    人気タグのグラデーションと半透明文字を他と統一
    - グラデ幅を他と統一してh-16に変更
    - 文字の半透明の濃さを80%から88%で他に合わせて、少し濃く見やすく

--- app/routes/($lang)._main._index/_components/home-tags-section.tsx
+++ app/routes/($lang)._main._index/_components/home-tags-section.tsx
@@ -40,7 +40,7 @@ export const HomeTagsSection = (props: Props) => {
                     src={tag.thumbnailUrl}
                     alt={tag.name}
                   />
-                  <div className="absolute right-0 bottom-0 left-0 box-border flex h-32 flex-col justify-end rounded bg-gradient-to-t from-black to-transparent p-4 pb-3 opacity-80">
+                  <div className="absolute right-0 bottom-0 left-0 box-border flex h-16 flex-col justify-end rounded bg-gradient-to-t from-black to-transparent p-4 pb-3 opacity-88">
                     <p className="text-white">{`#${tag.name}`}</p>
                   </div>
                 </Link>
