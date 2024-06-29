bbc3d5b930db1972e6ae72cbb475e1e5897ab8fb
syuribox <33959988+syuribox@users.noreply.github.com>
2024-06-30 03:01:11 +0900

    お題の「日」仮実装
    ・題だけだと寂しい
    ・後で消してもいいので、とりあえず実装

--- app/routes/($lang)._main.themes.$year.$month.$day._index/route.tsx
+++ app/routes/($lang)._main.themes.$year.$month.$day._index/route.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/_components/ui/button"
 import { AppPage } from "@/_components/app/app-page"
 import { dailyThemesQuery } from "@/_graphql/queries/daily-theme/daily-themes"
 import { createClient } from "@/_lib/client"
 import type { LoaderFunctionArgs } from "@remix-run/cloudflare"
-import { json, useParams } from "@remix-run/react"
+import { Link, json, useParams } from "@remix-run/react"
 import { useLoaderData } from "@remix-run/react"
 
 export async function loader(props: LoaderFunctionArgs) {
@@ -60,10 +61,57 @@ export default function Theme() {
 
   const data = useLoaderData<typeof loader>()
 
+  const year = Number.parseInt(params.year)
+  const month = Number.parseInt(params.month)
+  const day = Number.parseInt(params.day)
+  const tomorrow = new Date(year, month - 1, day)
+  tomorrow.setDate(tomorrow.getDate() + 1)
+  const yesterday = new Date(year, month - 1, day)
+  yesterday.setDate(yesterday.getDate() - 1)
+
   return (
     <AppPage>
       <article>
-        <h1>{data.dailyTheme.title}</h1>
+        <h1>お題</h1>
+        <div className="m-4">
+          <p className="text-lg md:text-xl">"{data.dailyTheme.title}"</p>
+          <Button asChild={true} className="m-1">
+            <Link to={`/tags/${data.dailyTheme.title}`}>
+              タグの作品一覧を見る
+            </Link>
+          </Button>
+          <p>
+            <Link to={`/themes/${year}/${month}/`} className="underline">
+              {year}年 {month}月
+            </Link>
+            {day}日
+          </p>
+          <p>
+            昨日：
+            <Button asChild={true} className="m-1">
+              <Link
+                to={`/themes/${yesterday.getFullYear()}/${
+                  yesterday.getMonth() + 1
+                }/${yesterday.getDate()}`}
+              >
+                {yesterday.getFullYear()}年 {yesterday.getMonth() + 1}月{" "}
+                {yesterday.getDate()}日
+              </Link>
+            </Button>
+            <br />
+            明日：
+            <Button asChild={true} className="m-1">
+              <Link
+                to={`/themes/${tomorrow.getFullYear()}/${
+                  tomorrow.getMonth() + 1
+                }/${tomorrow.getDate()}`}
+              >
+                {tomorrow.getFullYear()}年 {tomorrow.getMonth() + 1}月{" "}
+                {tomorrow.getDate()}日
+              </Link>
+            </Button>
+          </p>
+        </div>
       </article>
     </AppPage>
   )
