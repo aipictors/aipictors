61e5c484fa8903221c156eaece11d67e316b0133
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-07-14 15:08:56 +0900

    生成機のLoRAのコンポーネントの見た目を調整

--- app/routes/($lang).generation._index/_components/config-view/config-lora-model.tsx
+++ app/routes/($lang).generation._index/_components/config-view/config-lora-model.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/_components/ui/button"
+import { Card, CardContent } from "@/_components/ui/card"
 import { Input } from "@/_components/ui/input"
 import { Slider } from "@/_components/ui/slider"
+import { XIcon } from "lucide-react"
 
 type Props = {
   imageURL: string
@@ -13,44 +15,56 @@ type Props = {
 
 export const ConfigLoraModel = (props: Props) => {
   return (
-    <div className="flex space-x-2">
-      <img
-        className="w-16 rounded object-cover"
-        src={props.imageURL ?? ""}
-        alt={props.name}
-        draggable={false}
-      />
-      <div className="flex w-full flex-col gap-y-1 overflow-hidden">
-        <div className="flex flex-col">
-          <p className="whitespace-pre-wrap font-bold text-lg">{props.name}</p>
-          <p className="whitespace-pre-wrap text-sm">{props.description}</p>
+    <Card>
+      <CardContent className="flex items-center space-x-2 p-2">
+        <img
+          className="h-16 w-16 rounded object-cover"
+          src={props.imageURL ?? ""}
+          alt={props.name}
+          draggable={false}
+        />
+        <div className="flex w-full flex-col gap-y-1 overflow-hidden">
+          <div className="flex flex-col">
+            <div className="flex items-center gap-x-2">
+              <p className="whitespace-pre-wrap font-bold text-lg">
+                {props.name}
+              </p>
+              <Button
+                className="ml-auto"
+                size={"icon"}
+                variant={"ghost"}
+                onClick={props.onDelete}
+              >
+                <XIcon className="h-4 w-4" />
+              </Button>
+            </div>
+
+            <p className="whitespace-pre-wrap text-sm">{props.description}</p>
+          </div>
+          <div className="flex items-center gap-x-2">
+            <Slider
+              aria-label="slider-ex-2"
+              defaultValue={[props.value]}
+              min={-1}
+              max={1}
+              step={0.1}
+              onValueChange={(value) => {
+                props.setValue(value[0])
+              }}
+            />
+            <Input
+              type="number"
+              value={props.value.toFixed(2)}
+              className="w-20 font-bold"
+              min={-1}
+              max={1}
+              onChange={(event) => {
+                props.setValue(Number(event.target.value))
+              }}
+            />
+          </div>
         </div>
-        <div className="flex items-center gap-x-2">
-          <Slider
-            aria-label="slider-ex-2"
-            defaultValue={[props.value]}
-            min={-1}
-            max={1}
-            step={0.1}
-            onValueChange={(value) => {
-              props.setValue(value[0])
-            }}
-          />
-          <Input
-            type="number"
-            value={props.value.toFixed(2)}
-            className="w-20 font-bold"
-            min={-1}
-            max={1}
-            onChange={(event) => {
-              props.setValue(Number(event.target.value))
-            }}
-          />
-        </div>
-        <Button size={"sm"} variant={"secondary"} onClick={props.onDelete}>
-          {"削除"}
-        </Button>
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   )
 }
