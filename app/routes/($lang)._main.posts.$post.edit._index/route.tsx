a76c50ea1304a29f08f1913549a593f27be13422
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-07-16 22:19:14 +0900

    編集画面をリファクタする

--- app/routes/($lang)._main.posts.$post.edit._index/route.tsx
+++ app/routes/($lang)._main.posts.$post.edit._index/route.tsx
@@ -1,30 +1,595 @@
 import { ConstructionAlert } from "@/_components/construction-alert"
-import { ParamsError } from "@/_errors/params-error"
-import { useParams } from "@remix-run/react"
+import { Button } from "@/_components/ui/button"
+import { AuthContext } from "@/_contexts/auth-context"
+import { partialAlbumFieldsFragment } from "@/_graphql/fragments/partial-album-fields"
+import { partialUserFieldsFragment } from "@/_graphql/fragments/partial-user-fields"
+import { passFieldsFragment } from "@/_graphql/fragments/pass-fields"
+import { subWorkFieldsFragment } from "@/_graphql/fragments/sub-work-fields"
+import { userFieldsFragment } from "@/_graphql/fragments/user-fields"
+import { deleteUploadedImage } from "@/_utils/delete-uploaded-image"
+import { getSizeFromBase64 } from "@/_utils/get-size-from-base64"
+import { resizeImage } from "@/_utils/resize-image"
+import { sha256 } from "@/_utils/sha256"
+import { uploadPublicImage } from "@/_utils/upload-public-image"
+import { CreatingWorkDialog } from "@/routes/($lang)._main.new.image/_components/creating-work-dialog"
+import { PostImageFormInput } from "@/routes/($lang)._main.new.image/_components/post-image-form-input"
+import { PostImageFormUploader } from "@/routes/($lang)._main.new.image/_components/post-image-form-uploader"
+import { SuccessCreatedWorkDialog } from "@/routes/($lang)._main.new.image/_components/success-created-work-dialog"
+import { postImageFormInputReducer } from "@/routes/($lang)._main.new.image/reducers/post-image-form-input-reducer"
+import { postImageFormReducer } from "@/routes/($lang)._main.new.image/reducers/post-image-form-reducer"
+import { vPostImageForm } from "@/routes/($lang)._main.new.image/validations/post-image-form"
+import { useQuery, useMutation } from "@apollo/client/index"
+import { graphql } from "gql.tada"
+import { useContext, useReducer } from "react"
+import { toast } from "sonner"
+import { safeParse } from "valibot"
+import type { LoaderFunctionArgs } from "@remix-run/cloudflare"
+import { json, useLoaderData } from "@remix-run/react"
 
-export default function EditImage() {
-  const params = useParams()
-
-  if (params.post === undefined) {
-    throw new ParamsError()
+export async function loader(props: LoaderFunctionArgs) {
+  if (props.params.post === undefined) {
+    throw new Response(null, { status: 404 })
   }
 
+  return json({
+    id: props.params.post,
+  })
+}
+
+export default function EditImage() {
+  const authContext = useContext(AuthContext)
+
+  const data = useLoaderData<typeof loader>()
+
+  const { data: work = null } = useQuery(workQuery, {
+    skip: authContext.isLoading,
+    variables: {
+      id: data.id,
+    },
+  })
+
+  const [state, dispatch] = useReducer(postImageFormReducer, {
+    editTargetImageBase64: null,
+    indexList: [],
+    isDrawing: false,
+    isHovered: false,
+    isOpenImageGenerationDialog: false,
+    isSelectedGenerationImage: false,
+    isThumbnailLandscape:
+      (work?.work?.smallThumbnailImageWidth ?? 0) >
+      (work?.work?.smallThumbnailImageHeight ?? 0),
+    items: [
+      {
+        id: 0,
+        content: work?.work?.imageURL ?? "",
+      },
+      ...(work?.work?.subWorks ?? []).map((subWork, index) => ({
+        id: index + 1,
+        content: subWork.imageUrl ?? "",
+      })),
+    ],
+    ogpBase64: work?.work?.ogpThumbnailImageUrl ?? "",
+    pngInfo: {
+      src: work?.work?.pngInfo ?? "",
+      params: {
+        prompt: work?.work?.prompt ?? "",
+        negativePrompt: work?.work?.negativePrompt ?? "",
+        seed: work?.work?.seed?.toString() ?? "",
+        sampler: work?.work?.sampler ?? "",
+        strength: work?.work?.strength ?? "",
+        noise: work?.work?.noise ?? "",
+        model: work?.work?.model ?? "",
+        modelHash: work?.work?.modelHash ?? "",
+        steps: work?.work?.steps ? work?.work?.steps.toString() : "",
+        scale: work?.work?.scale ? work?.work?.scale.toString() : "",
+        vae: "",
+      },
+    },
+    progress: 0,
+    selectedImageGenerationIds: [],
+    thumbnailBase64: work?.work?.largeThumbnailImageURL ?? "",
+    thumbnailPosX:
+      work?.work?.smallThumbnailImageWidth ??
+      0 > (work?.work?.smallThumbnailImageHeight ?? 0)
+        ? work?.work?.thumbnailImagePosition ?? 0
+        : 0,
+    thumbnailPosY:
+      work?.work?.smallThumbnailImageWidth ??
+      0 > (work?.work?.smallThumbnailImageHeight ?? 0) ??
+      0
+        ? 0
+        : work?.work?.thumbnailImagePosition ?? 0,
+    uploadedWorkId: null,
+    uploadedWorkUuid: null,
+    videoFile: null,
+  })
+
+  const [inputState, dispatchInput] = useReducer(postImageFormInputReducer, {
+    accessType: work?.work?.accessType ?? "PUBLIC",
+    generationParamAccessType:
+      work?.work?.promptAccessType === "PUBLIC" ? "PUBLIC" : "PRIVATE",
+    aiModelId: work?.work?.generationModelId ?? null,
+    albumId: work?.work?.album?.id ?? null,
+    caption: work?.work?.description ?? "",
+    date: new Date(work?.work?.createdAt ?? new Date()),
+    enCaption: work?.work?.enDescription ?? "",
+    enTitle: work?.work?.enTitle ?? "",
+    imageInformation: {
+      params: {
+        prompt: work?.work?.prompt ?? "",
+        negativePrompt: work?.work?.negativePrompt ?? "",
+        seed: work?.work?.seed?.toString() ?? "",
+        steps: work?.work?.steps ? work?.work?.steps.toString() : "",
+        strength: work?.work?.strength ?? "",
+        noise: work?.work?.noise ?? "",
+        scale: work?.work?.scale ? work?.work?.scale.toString() : "",
+        sampler: work?.work?.sampler ?? "",
+        vae: work?.work?.vae ?? "",
+        modelHash: work?.work?.modelHash ?? "",
+        model: work?.work?.model ?? "",
+      },
+      src: work?.work?.pngInfo ?? "",
+    },
+    imageStyle: work?.work?.style ?? "ILLUSTRATION",
+    link: work?.work?.relatedUrl ?? "",
+    ratingRestriction: work?.work?.rating ?? "G",
+    reservationDate: null,
+    reservationTime: null,
+    tags: [
+      ...(work?.work?.tagNames.map((tag) => ({
+        id: tag,
+        text: tag,
+      })) ?? []),
+    ],
+    themeId: work?.work?.dailyTheme?.id ?? null,
+    title: work?.work?.title ?? "",
+    useCommentFeature: work?.work?.isCommentsEditable ?? true,
+    useGenerationParams: work?.work?.isGeneration ?? false,
+    usePromotionFeature: work?.work?.isPromotion ?? false,
+    useTagFeature: work?.work?.isTagEditable ?? true,
+  })
+
+  const { data: viewer } = useQuery(viewerQuery, {
+    skip: authContext.isNotLoggedIn,
+    variables: {
+      offset: 0,
+      limit: 128,
+      ownerUserId: authContext.userId,
+    },
+  })
+
+  const [updateWork, { loading: isUpdatedLoading }] =
+    useMutation(updateWorkMutation)
+
+  const formResult = safeParse(vPostImageForm, {
+    title: inputState.title,
+    caption: inputState.caption,
+    enTitle: inputState.enTitle,
+    enCaption: inputState.enCaption,
+    imagesCount: state.items.length,
+    thumbnailBase64: state.thumbnailBase64,
+  })
+
+  const uploadImages = async () => {
+    if (authContext.userId === null) {
+      return []
+    }
+    const images = state.items.map((item) => item.content)
+    const uploads = images.map((image) => {
+      if (image === null) {
+        return null
+      }
+      return uploadPublicImage(image, viewer?.viewer?.token)
+    })
+    const imageUrls = await Promise.all(uploads)
+    return imageUrls
+  }
+
+  const onPost = async () => {
+    if (formResult.success === false) {
+      for (const issue of formResult.issues) {
+        toast(issue.message)
+        return
+      }
+      return
+    }
+
+    if (
+      inputState.reservationDate !== null &&
+      inputState.reservationTime === null
+    ) {
+      toast("予約投稿の時間を入力してください")
+      return
+    }
+
+    if (
+      inputState.reservationDate === null &&
+      inputState.reservationTime !== null
+    ) {
+      toast("予約投稿の時間を入力してください")
+      return
+    }
+
+    const uploadedImageUrls = []
+
+    try {
+      dispatch({ type: "SET_PROGRESS", payload: 10 })
+
+      const smallThumbnail = state.isThumbnailLandscape
+        ? await resizeImage(formResult.output.thumbnailBase64, 400, 0, "webp")
+        : await resizeImage(formResult.output.thumbnailBase64, 0, 400, "webp")
+
+      dispatch({ type: "SET_PROGRESS", payload: 20 })
+
+      const largeThumbnail = state.isThumbnailLandscape
+        ? await resizeImage(formResult.output.thumbnailBase64, 600, 0, "webp")
+        : await resizeImage(formResult.output.thumbnailBase64, 0, 600, "webp")
+
+      dispatch({ type: "SET_PROGRESS", payload: 30 })
+
+      const smallThumbnailUrl = await uploadPublicImage(
+        smallThumbnail.base64,
+        viewer?.viewer?.token,
+      )
+
+      dispatch({ type: "SET_PROGRESS", payload: 40 })
+
+      uploadedImageUrls.push(smallThumbnailUrl)
+
+      const largeThumbnailUrl = await uploadPublicImage(
+        largeThumbnail.base64,
+        viewer?.viewer?.token,
+      )
+
+      dispatch({ type: "SET_PROGRESS", payload: 50 })
+
+      uploadedImageUrls.push(largeThumbnailUrl)
+
+      const ogpBase64Url = state.ogpBase64
+        ? await uploadPublicImage(state.ogpBase64, viewer?.viewer?.token)
+        : null
+
+      dispatch({ type: "SET_PROGRESS", payload: 60 })
+
+      if (ogpBase64Url !== null) {
+        uploadedImageUrls.push(ogpBase64Url)
+      }
+
+      const reservedAt =
+        inputState.reservationDate !== null &&
+        inputState.reservationTime !== null
+          ? new Date(
+              `${inputState.reservationDate}T${inputState.reservationTime}`,
+            ).getTime() +
+            3600000 * 9
+          : undefined
+
+      const mainImageSha256 = await sha256(formResult.output.thumbnailBase64)
+
+      const mainImageSize = await getSizeFromBase64(
+        formResult.output.thumbnailBase64,
+      )
+
+      dispatch({ type: "SET_PROGRESS", payload: 70 })
+
+      const uploadResults = await uploadImages()
+
+      const imageUrls = uploadResults.filter((url) => url !== null)
+
+      if (imageUrls.length === 0) {
+        toast("画像のアップロードに失敗しました")
+        return
+      }
+
+      const work = await updateWork({
+        variables: {
+          input: {
+            id: data.id,
+            title: formResult.output.title,
+            entitle: formResult.output.enTitle,
+            explanation: formResult.output.caption,
+            enExplanation: formResult.output.enCaption,
+            rating: inputState.ratingRestriction,
+            prompt: state.pngInfo?.params.prompt ?? null,
+            negativePrompt: state.pngInfo?.params.negativePrompt ?? null,
+            seed: state.pngInfo?.params.seed ?? null,
+            sampler: state.pngInfo?.params.sampler ?? null,
+            strength: state.pngInfo?.params.strength ?? null,
+            noise: state.pngInfo?.params.noise ?? null,
+            modelName: state.pngInfo?.params.model ?? null,
+            modelHash: state.pngInfo?.params.modelHash ?? null,
+            otherGenerationParams: state.pngInfo?.src ?? null,
+            pngInfo: state.pngInfo?.src ?? null,
+            imageStyle: inputState.imageStyle,
+            relatedUrl: inputState.link,
+            tags: inputState.tags.map((tag) => tag.text),
+            isTagEditable: inputState.useTagFeature,
+            isCommentEditable: inputState.useCommentFeature,
+            thumbnailPosition: state.isThumbnailLandscape
+              ? state.thumbnailPosX
+              : state.thumbnailPosY,
+            modelId: inputState.aiModelId,
+            type: "WORK",
+            subjectId: inputState.themeId,
+            albumId: inputState.albumId,
+            isPromotion: inputState.usePromotionFeature,
+            reservedAt: reservedAt,
+            mainImageSha256: mainImageSha256,
+            accessType: inputState.accessType,
+            imageUrls: imageUrls,
+            smallThumbnailImageURL: smallThumbnailUrl,
+            smallThumbnailImageWidth: smallThumbnail.width,
+            smallThumbnailImageHeight: smallThumbnail.height,
+            largeThumbnailImageURL: largeThumbnailUrl,
+            largeThumbnailImageWidth: largeThumbnail.width,
+            largeThumbnailImageHeight: largeThumbnail.height,
+            videoUrl: null,
+            ogpImageUrl: ogpBase64Url,
+            imageHeight: mainImageSize.height,
+            imageWidth: mainImageSize.width,
+            accessGenerationType:
+              state.isSelectedGenerationImage && inputState.useGenerationParams
+                ? "PUBLIC_IN_OWN_PRODUCT"
+                : inputState.useGenerationParams
+                  ? "PUBLIC"
+                  : "PRIVATE",
+          },
+        },
+      })
+
+      if (work?.data?.updateWork === undefined) {
+        toast("作品の更新に失敗しました")
+        return
+      }
+
+      dispatch({
+        type: "MARK_AS_DONE",
+        payload: {
+          uploadedWorkId: work?.data?.updateWork.id,
+          uploadedWorkUuid:
+            inputState.accessType !== "PRIVATE"
+              ? null
+              : work?.data?.updateWork.uuid ?? null,
+        },
+      })
+
+      toast("作品を投稿しました")
+    } catch (error) {
+      if (error instanceof Error) {
+        toast(error.message)
+      }
+      const promises = uploadedImageUrls.map((url) => {
+        return deleteUploadedImage(url)
+      })
+      await Promise.all(promises)
+      dispatch({ type: "SET_PROGRESS", payload: 0 })
+    }
+  }
+
+  const createdAt = new Date(
+    `${inputState.reservationDate}T${inputState.reservationTime}`,
+  )
+
+  // TODO_2024_08: 仕組みが分からなかった
+  // useBeforeUnload(
+  //   React.useCallback(
+  //     (event) => {
+  //       if (state.state) {
+  //         const confirmationMessage =
+  //           "ページ遷移すると変更が消えますが問題無いですか？"
+  //         event.returnValue = confirmationMessage
+  //         return confirmationMessage
+  //       }
+  //     },
+  //     [state.state],
+  //   ),
+  // )
+
   return (
-    <div
-      className="container space-y-2"
-      style={{
-        margin: "0 auto",
-      }}
-    >
+    <div className="m-auto w-full max-w-[1200px] space-y-2">
       <ConstructionAlert
         type="WARNING"
         title="試験的にリニューアル版を運用中です。"
-        fallbackURL={`https://www.aipictors.com/edit-work/?id=${params.post}`}
+        fallbackURL="https://www.aipictors.com/post"
       />
-
-      {/* <Suspense>
-        <EditImageForm workId={params.post} />
-      </Suspense> */}
+      <div className="space-y-4">
+        <PostImageFormUploader state={state} dispatch={dispatch} />
+        <PostImageFormInput
+          imageInformation={state.pngInfo}
+          state={inputState}
+          dispatch={dispatchInput}
+          albums={viewer?.albums ?? []}
+          currentPass={viewer?.viewer?.currentPass ?? null}
+        />
+        <Button size={"lg"} className="w-full" type="submit" onClick={onPost}>
+          {"投稿"}
+        </Button>
+      </div>
+      <SuccessCreatedWorkDialog
+        isOpen={state.progress === 100}
+        title={inputState.title}
+        imageBase64={state.thumbnailBase64}
+        workId={state.uploadedWorkId}
+        uuid={state.uploadedWorkUuid}
+        shareTags={["Aipictors", "AIイラスト", "AIart"]}
+        createdAt={createdAt.getTime()}
+      />
+      <CreatingWorkDialog
+        progress={state.progress}
+        isOpen={state.progress !== 0 && state.progress !== 100}
+      />
     </div>
   )
 }
+
+const viewerQuery = graphql(
+  `query ViewerQuery(
+    $limit: Int!,
+    $offset: Int!,
+    $ownerUserId: ID
+  ) {
+    viewer {
+      id
+      token
+      user {
+        id
+        nanoid
+        hasSignedImageGenerationTerms
+      }
+      currentPass {
+        ...PassFields
+      }
+    }
+    albums(
+      offset: $offset,
+      limit: $limit,
+      where: {
+        ownerUserId: $ownerUserId,
+        isSensitiveAndAllRating: true,
+        needInspected: false,
+        needsThumbnailImage: false,
+      }
+    ) {
+      ...PartialAlbumFields
+      user {
+        ...PartialUserFields
+      }
+    }
+  }`,
+  [partialAlbumFieldsFragment, partialUserFieldsFragment, passFieldsFragment],
+)
+
+const workQuery = graphql(
+  `query Work($id: ID!) {
+    work(id: $id) {
+      id
+      isMyRecommended
+      title
+      accessType
+      type
+      adminAccessType
+      promptAccessType
+      rating
+      description
+      isSensitive
+      enTitle
+      enDescription
+      imageURL
+      largeThumbnailImageURL
+      largeThumbnailImageWidth
+      largeThumbnailImageHeight
+      smallThumbnailImageURL
+      smallThumbnailImageWidth
+      smallThumbnailImageHeight
+      thumbnailImagePosition
+      subWorksCount
+      user {
+        id
+        promptonUser {
+          id
+        }
+        ...UserFields
+        isFollower
+        isFollowee
+        isMuted
+        works(offset: 0, limit: 16) {
+          id
+          userId
+          largeThumbnailImageURL
+          largeThumbnailImageWidth
+          largeThumbnailImageHeight
+          smallThumbnailImageURL
+          smallThumbnailImageWidth
+          smallThumbnailImageHeight
+          thumbnailImagePosition
+          subWorksCount
+        }
+      }
+      likedUsers(offset: 0, limit: 32) {
+        id
+        name
+        iconUrl
+        login
+      }
+      album {
+        id
+        title
+        description
+      }
+      dailyTheme {
+        id
+        title
+      }
+      tagNames
+      createdAt
+      likesCount
+      viewsCount
+      commentsCount
+      subWorks {
+        ...SubWorkFields
+      }
+      nextWork {
+        id
+        smallThumbnailImageURL
+        smallThumbnailImageWidth
+        smallThumbnailImageHeight
+        thumbnailImagePosition
+      }
+      previousWork {
+        id
+        smallThumbnailImageURL
+        smallThumbnailImageWidth
+        smallThumbnailImageHeight
+        thumbnailImagePosition
+      }
+      model
+      modelHash
+      generationModelId
+      workModelId
+      isTagEditable
+      isCommentsEditable
+      isLiked
+      isBookmarked
+      isInCollection
+      isPromotion
+      isGeneration
+      ogpThumbnailImageUrl
+      prompt
+      negativePrompt
+      noise
+      seed
+      steps
+      sampler
+      scale
+      strength
+      vae
+      clipSkip
+      otherGenerationParams
+      pngInfo
+      style
+      url
+      html
+      updatedAt
+      dailyRanking
+      weeklyRanking
+      monthlyRanking
+      relatedUrl
+      nanoid
+    }
+  }`,
+  [userFieldsFragment, subWorkFieldsFragment],
+)
+
+const updateWorkMutation = graphql(
+  `mutation UpdateWork($input: UpdateWorkInput!) {
+    updateWork(input: $input) {
+      id
+      title
+      description
+      rating
+      accessType
+      nanoid
+      uuid
+    }
+  }`,
+)
