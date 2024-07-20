3ed6675f4c8046994f57bd08061ec18d59bb5db5
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-07-20 22:09:03 +0900

    merge main

---
+++ app/routes/($lang)._main.releases.$id._index/_components/release-item.tsx
@@ -0,0 +1,33 @@
+type Props = {
+  title: string
+  description: string
+  thumbnailUrl: string
+  platform: string
+  createdAt: number
+}
+
+/**
+ * リリース情報アイテム
+ */
+export const ReleaseItem = (props: Props) => {
+  const { title, description, thumbnailUrl, platform, createdAt } = props
+
+  const formatDate = (timestamp: number) => {
+    const date = new Date(timestamp)
+    return date.toLocaleDateString()
+  }
+
+  return (
+    <>
+      <img
+        src={thumbnailUrl}
+        alt={title}
+        className="m-auto mb-4 h-32 w-full max-w-64 rounded object-cover"
+      />
+      <div className="font-bold text-lg">{title}</div>
+      <div className="mb-2 text-gray-500 text-sm">{platform}</div>
+      <div className="text-sm">{description}</div>
+      <div className="mt-4 text-gray-400 text-xs">{formatDate(createdAt)}</div>
+    </>
+  )
+}
---
+++ app/routes/($lang)._main.releases._index/route.tsx
@@ -0,0 +1,96 @@
+import { json, Link, useLoaderData } from "@remix-run/react"
+import { microCmsClient } from "@/_lib/client"
+import { useState } from "react"
+import { Button } from "@/_components/ui/button"
+import type { LoaderFunctionArgs } from "@remix-run/cloudflare"
+
+interface Release {
+  id: string
+  title: string
+  description: string
+  thumbnail_url: {
+    url: string
+  }
+  platform: string
+  createdAt: number
+}
+
+interface ApiResponse {
+  contents: Release[]
+  totalCount: number
+  offset: number
+  limit: number
+}
+
+export async function loader(props: LoaderFunctionArgs) {
+  const limit = 16
+  const offset = props.params.offset ? Number(props.params.offset) : 0
+
+  const data: ApiResponse = await microCmsClient.get({
+    endpoint: `releases?orders=createdAt&limit=${limit}&offset=${offset}`,
+  })
+
+  return json({
+    data,
+  })
+}
+
+export default function Milestone() {
+  const data = useLoaderData<typeof loader>()
+  const [limit] = useState(10)
+  const [offset, setOffset] = useState(data.data.offset)
+
+  const releases: Release[] = data.data.contents
+
+  const handleNext = () => {
+    setOffset(offset + limit)
+  }
+
+  const handlePrevious = () => {
+    if (offset - limit >= 0) {
+      setOffset(offset - limit)
+    }
+  }
+
+  const formatDate = (timestamp: number) => {
+    const date = new Date(timestamp)
+    return date.toLocaleDateString()
+  }
+
+  return (
+    <div className="container flex flex-col space-y-4">
+      <h2 className="border-b p-4">最新リリース一覧</h2>
+      {releases.map((release) => (
+        <Link
+          to={`/releases/${release.id}`}
+          key={release.createdAt}
+          className="flex items-center space-x-4 border-b p-4"
+        >
+          <img
+            src={release.thumbnail_url.url}
+            alt={release.title}
+            className="h-16 w-16 rounded-md object-cover"
+          />
+          <div className="flex flex-1 flex-col">
+            <div className="flex justify-between">
+              <p className="font-bold text-lg">{release.title}</p>
+              <p className="text-sm">{formatDate(release.createdAt)}</p>
+            </div>
+            <p className="line-clamp-2 text-sm">{release.description}</p>
+          </div>
+        </Link>
+      ))}
+      <div className="flex items-center justify-center space-x-4 pt-4">
+        <Button onClick={handlePrevious} disabled={offset === 0}>
+          前へ
+        </Button>
+        <Button
+          onClick={handleNext}
+          disabled={offset + limit >= data.data.totalCount}
+        >
+          次へ
+        </Button>
+      </div>
+    </div>
+  )
+}
