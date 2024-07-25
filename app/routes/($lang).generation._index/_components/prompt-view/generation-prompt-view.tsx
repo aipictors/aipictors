9ceff42aafed063126d46c362827507e4b388cb1
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-07-25 10:30:39 +0900

    [fix] キーワード入力欄のデザイン修正

--- app/routes/($lang).generation._index/_components/prompt-view/generation-prompt-view.tsx
+++ app/routes/($lang).generation._index/_components/prompt-view/generation-prompt-view.tsx
@@ -1,6 +1,7 @@
 import { AutoResizeTextarea } from "@/_components/auto-resize-textarea"
 import { Button } from "@/_components/ui/button"
 import { Dialog, DialogTrigger } from "@/_components/ui/dialog"
+import { Textarea } from "@/_components/ui/textarea"
 import { GenerationViewCard } from "@/routes/($lang).generation._index/_components/generation-view-card"
 import { PromptCategoriesDialogContent } from "@/routes/($lang).generation._index/_components/prompt-view/prompt-categories-dialog-content"
 import { useGenerationContext } from "@/routes/($lang).generation._index/_hooks/use-generation-context"
@@ -70,8 +71,21 @@ export const GenerationPromptView = () => {
         }
       >
         <div className="relative flex h-full flex-col gap-y-2 px-4 pb-1 md:pb-4">
+          <Textarea
+            className="hidden h-full min-h-48 resize-none font-mono md:block md:min-h-16"
+            placeholder={
+              "生成したいイラストの要素をキーワードで入力してください。例: 1 girl, masterpiece"
+            }
+            value={context.config.promptText}
+            onChange={(event) => {
+              context.updatePrompt(event.target.value)
+            }}
+            onBlur={() => {
+              context.initPromptWithLoraModel()
+            }}
+          />
           <AutoResizeTextarea
-            className="h-full min-h-48 resize-none font-mono md:min-h-16"
+            className="block h-full min-h-48 resize-none font-mono md:hidden md:min-h-16"
             placeholder={"生成キーワード例: 1 girl, masterpiece"}
             value={context.config.promptText}
             onChange={(event) => {
@@ -82,7 +96,6 @@ export const GenerationPromptView = () => {
             }}
             minHeight="120px"
           />
-
           <Dialog
             open={value}
             onOpenChange={(isOpen) => {
