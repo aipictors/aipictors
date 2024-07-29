2f063a2020892eeddf6424b3973df1876a15d14f
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-07-30 02:54:15 +0900

    [add] ダッシュボードの作品一覧を開けるように修正、作品の下書きページを追加

---
+++ app/routes/($lang)._main.posts.$post.draft/route.tsx
@@ -0,0 +1,80 @@
+import { ParamsError } from "~/errors/params-error"
+import { createClient } from "~/lib/client"
+import { workArticleFragment } from "~/routes/($lang)._main.posts.$post/components/work-article"
+import { commentFragment } from "~/routes/($lang)._main.posts.$post/components/work-comment-list"
+import { WorkContainer } from "~/routes/($lang)._main.posts.$post/components/work-container"
+import type { LoaderFunctionArgs } from "@remix-run/cloudflare"
+import { json, useParams } from "@remix-run/react"
+import { useLoaderData } from "@remix-run/react"
+import { graphql } from "gql.tada"
+
+export async function loader(props: LoaderFunctionArgs) {
+  if (props.params.post === undefined) {
+    throw new Response(null, { status: 404 })
+  }
+
+  const client = createClient()
+
+  const workResp = await client.query({
+    query: workQuery,
+    variables: {
+      id: props.params.post,
+    },
+  })
+
+  const workCommentsResp = await client.query({
+    query: workCommentsQuery,
+    variables: {
+      workId: props.params.post,
+    },
+  })
+
+  if (workResp.data.work === null) {
+    throw new Response(null, { status: 404 })
+  }
+
+  if (workCommentsResp.data.work === null) {
+    throw new Response(null, { status: 404 })
+  }
+
+  // 作品と同じ年齢種別で新着順の作品一覧を取得
+  const rating = workResp.data.work.rating
+
+  return json({
+    work: workResp.data.work,
+    workComments: workCommentsResp.data.work.comments,
+  })
+}
+
+export default function Work() {
+  const params = useParams()
+
+  if (params.post === undefined) {
+    throw new ParamsError()
+  }
+
+  const data = useLoaderData<typeof loader>()
+
+  return <WorkContainer work={data.work} comments={data.workComments} />
+}
+
+const workCommentsQuery = graphql(
+  `query WorkComments($workId: ID!) {
+    work(id: $workId) {
+      id
+      comments(offset: 0, limit: 128) {
+        ...Comment
+      }
+    }
+  }`,
+  [commentFragment],
+)
+
+const workQuery = graphql(
+  `query Work($id: ID!) {
+    work(id: $id) {
+      ...WorkArticle
+    }
+  }`,
+  [workArticleFragment],
+)
