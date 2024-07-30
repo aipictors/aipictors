4445f06b1972567e6570acf90959f69d140f4bbf
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-07-30 21:36:39 +0900

    [fix] 画像ドラッグ後のインデックスがずれるバグを修正

--- app/components/drag/sortable-items.tsx
+++ app/components/drag/sortable-items.tsx
@@ -36,6 +36,7 @@ type Props = {
  */
 export const SortableItems = (props: Props) => {
   const [activeItem, setActiveItem] = useState<TSortableItem>()
+  const [isDragging, setIsDragging] = useState(false)
 
   const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor))
 
@@ -43,6 +44,7 @@ export const SortableItems = (props: Props) => {
     const { active } = event
     console.log("Drag started for item with id:", active.id) // ログ出力
     setActiveItem(props.items.find((item) => item.id === active.id))
+    setIsDragging(true)
   }
 
   const handleDragOver = (event: DragOverEvent) => {
@@ -60,7 +62,47 @@ export const SortableItems = (props: Props) => {
   }
 
   const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event
+    if (!over) return
+
+    const activeItem = props.items.find((item) => item.id === active.id)
+    const overItem = props.items.find((item) => item.id === over.id)
+
+    if (!activeItem || !overItem) {
+      return
+    }
+
+    const activeIndex = props.items.findIndex((item) => item.id === active.id)
+    const overIndex = props.items.findIndex((item) => item.id === over.id)
+    const newItems =
+      activeIndex !== overIndex
+        ? arrayMove<TSortableItem>(props.items, activeIndex, overIndex)
+        : props.items
     setActiveItem(undefined)
+    setIsDragging(false)
+    props.setItems(
+      newItems.map((item, index) => ({
+        ...item,
+        id: index,
+      })),
+    )
+    // インデックス並び替え
+    changeIndexList(activeIndex, overIndex)
+  }
+
+  const changeIndexList = (activeIndex: number, overIndex: number) => {
+    const newIds = props.items
+      .map((item) => item.id)
+      .map((id) => {
+        if (id === activeIndex) {
+          return overIndex
+        }
+        if (id === overIndex) {
+          return activeIndex
+        }
+        return id
+      })
+    props.setIndexList(newIds)
   }
 
   const deleteIndex = (deletedId: number) => {
@@ -106,9 +148,11 @@ export const SortableItems = (props: Props) => {
           {props.dummyEnableDragItem && <div>{props.dummyEnableDragItem}</div>}
         </div>
       </SortableContext>
-      <DragOverlay adjustScale style={{ transformOrigin: "0 0 " }}>
-        {activeItem ? <ImageItem item={activeItem} isDragging /> : null}
-      </DragOverlay>
+      {isDragging && (
+        <DragOverlay adjustScale style={{ transformOrigin: "0 0 " }}>
+          {activeItem ? <ImageItem item={activeItem} isDragging /> : null}
+        </DragOverlay>
+      )}
     </DndContext>
   )
 }
