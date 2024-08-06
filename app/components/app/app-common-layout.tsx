51c66c7819be99c40e26c22aecf946d71f43b021
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-08-06 20:04:05 +0900

    [fix] 前回のサイドメニューの開閉状態を記憶するようにする

--- app/components/app/app-common-layout.tsx
+++ app/components/app/app-common-layout.tsx
@@ -2,7 +2,9 @@ import { AppAside } from "~/components/app/app-aside"
 import { AppContents } from "~/components/app/app-contents"
 import HomeHeader from "~/routes/($lang)._main._index/components/home-header"
 import { HomeRouteList } from "~/routes/($lang)._main._index/components/home-route-list"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { setCookie } from "~/utils/set-cookie"
+import { getCookie } from "~/utils/get-cookie"
 
 type Props = Readonly<{
   outlet: React.ReactNode
@@ -15,18 +17,22 @@ type Props = Readonly<{
 export function AppCommonLayout(props: Props) {
   const [isOpen, setIsOpen] = useState(false)
 
-  const close = () => {
-    setIsOpen(false)
+  useEffect(() => {
+    const size = getCookie("size-common-menu")
+    if (size !== null) {
+      setIsOpen(Number(size) === 1)
+    }
+  }, [])
+
+  const onToggleSideMenu = () => {
+    const newIsOpen = !isOpen
+    setIsOpen(newIsOpen)
+    setCookie("size-common-menu", newIsOpen ? 1 : 0)
   }
 
   return (
     <>
-      <HomeHeader
-        onToggleSideMenu={() => {
-          setIsOpen(!isOpen)
-        }}
-        title={props.title}
-      />
+      <HomeHeader onToggleSideMenu={onToggleSideMenu} title={props.title} />
       <AppContents
         aside={
           <AppAside isOpen={isOpen}>
