2f1445c2dc9f71cff3a52cba835110f533e1eef7
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-08-07 20:30:29 +0900

    コラム、小説についてHTML表示に対応する

--- app/routes/($lang)._main.posts.$post/components/work-markdown-view.tsx
+++ app/routes/($lang)._main.posts.$post/components/work-markdown-view.tsx
@@ -7,12 +7,13 @@ import HorizontalRule from "@tiptap/extension-horizontal-rule"
 import Image from "@tiptap/extension-image"
 
 type Props = {
-  md: string
+  md?: string
+  html?: string
   thumbnailUrl: string
   title: string
 }
 
-export const WorkMarkdownView = ({ thumbnailUrl, md, title }: Props) => {
+export const WorkMarkdownView = (props: Props) => {
   const editor = useEditor({
     editable: false,
     editorProps: {
@@ -48,31 +49,43 @@ export const WorkMarkdownView = ({ thumbnailUrl, md, title }: Props) => {
       Blockquote,
       HorizontalRule,
     ],
-    content: md,
+    content: props.md,
   })
+
+  console.log(props.html)
 
   return (
     <div className="relative m-0">
       <div className="relative h-64 w-full overflow-hidden rounded-lg md:h-80">
         <img
-          src={thumbnailUrl}
+          src={props.thumbnailUrl}
           alt="Thumbnail Background"
           className="absolute inset-0 h-full w-full object-cover"
         />
         <div className="relative flex h-full w-full items-center justify-center bg-black bg-opacity-40">
           <img
-            src={thumbnailUrl}
+            src={props.thumbnailUrl}
             alt="Thumbnail Foreground"
             className="h-3/4 w-3/4 object-contain"
           />
           <div className="absolute right-4 bottom-4 left-4 rounded-lg bg-black bg-opacity-50 p-4 text-center font-bold text-2xl text-white md:text-4xl">
-            {title}
+            {props.title}
           </div>
         </div>
       </div>
       <div className="mt-4 rounded-lg bg-white p-4 dark:bg-zinc-900">
         <EditorContent editor={editor} />
       </div>
+      {props.html && (
+        <div
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+          dangerouslySetInnerHTML={{
+            __html: props.html
+              .replace(/&quot;/g, '"') // &quot; を " に置換
+              .replace(/\\"/g, '"'), // \\" を " に置換
+          }}
+        />
+      )}
     </div>
   )
 }
