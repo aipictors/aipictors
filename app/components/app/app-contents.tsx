3d8428365abab48f9a47229f2888ba960d90984b
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-08-07 21:00:01 +0900

    Update app-contents.tsx

--- app/components/app/app-contents.tsx
+++ app/components/app/app-contents.tsx
@@ -14,7 +14,7 @@ export function AppContents(props: Props) {
         {props.aside && props.aside}
         <div
           // className={`container m-auto space-y-4 overflow-auto pb-4 ${props.isOpen ? "px-4 md:pr-8 md:pl-52" : "px-0 md:px-8"}`}
-          className={`m-auto w-full space-y-4 overflow-auto pb-4 ${props.isOpen ? "px-4 md:pr-8 md:pl-52" : "px-0 md:px-8"}`}
+          className={`m-auto w-full space-y-4 overflow-auto pb-4 ${props.isOpen ? "px-4 md:pr-8 md:pl-52" : "px-4 md:px-8"}`}
         >
           {props.outlet}
         </div>
