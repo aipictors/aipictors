58a540c6e117f47c36f1c5d81ec2a23d4b851ba7
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-08-12 00:52:25 +0900

    作品一覧について画像の高さが反映されない不具合を修正

--- app/components/responsive-photo-video-works-album.tsx
+++ app/components/responsive-photo-video-works-album.tsx
@@ -149,7 +149,7 @@ export const ResponsivePhotoVideoWorksAlbum = (props: Props) => {
                       src={context.photo.url ?? ""}
                       // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
                       ref={(el) => (videoRefs.current[context.index] = el)}
-                      className="absolute top-0 left-0 h-full overflow-hidden rounded"
+                      className="absolute top-0 left-0 w-full overflow-hidden rounded object-contain"
                       style={isAutoPlay ? { zIndex: "10" } : { zIndex: "-1" }}
                       muted
                       autoPlay={isAutoPlay}
@@ -161,6 +161,7 @@ export const ResponsivePhotoVideoWorksAlbum = (props: Props) => {
                         label="English"
                       />
                     </video>
+
                     <div className="absolute top-1 left-1 opacity-50">
                       <Badge variant={"secondary"} className="text-xs">
                         {"video"}
