474d5797beec0d352955dda46561f87caa77fa57
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-08-16 13:40:04 +0900

    Update home-banners.tsx

--- app/routes/($lang)._main._index/components/home-banners.tsx
+++ app/routes/($lang)._main._index/components/home-banners.tsx
@@ -32,20 +32,20 @@ export function HomeBanners(props: Props) {
           </Link>
         </CarouselItem>
         <CarouselItem className="basis-1/1 xl:basis-1/1">
-          <Link to="/generation">
-            <img
-              alt="home_banner_01"
-              className="h-40 w-auto md:h-64 md:max-h-auto"
-              src="https://assets.aipictors.com/home_banner_02.webp"
-            />
-          </Link>
-        </CarouselItem>
-        <CarouselItem className="basis-1/1 xl:basis-1/1">
           <Link to="/events/halloween-2024">
             <img
               alt="home_banner_01"
               className="h-40 w-auto md:h-64 md:max-h-auto"
               src="https://assets.aipictors.com/wakiaiai-3-halloween-2024-top-banner.webp"
+            />
+          </Link>
+        </CarouselItem>{" "}
+        <CarouselItem className="basis-1/1 xl:basis-1/1">
+          <Link to="/generation">
+            <img
+              alt="home_banner_01"
+              className="h-40 w-auto md:h-64 md:max-h-auto"
+              src="https://assets.aipictors.com/home_banner_02.webp"
             />
           </Link>
         </CarouselItem>
