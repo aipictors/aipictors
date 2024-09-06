3b4c97281adee619fe6ad7b96eaae0455e292381
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-09-06 18:31:14 +0900

    Update setting-advertisements-form.tsx

--- app/routes/($lang).settings.advertisements/_components/setting-advertisements-form.tsx
+++ app/routes/($lang).settings.advertisements/_components/setting-advertisements-form.tsx
@@ -345,36 +345,38 @@ export function SettingAdvertisementsForm() {
             />
             <Button onClick={handleSearch}>検索</Button>
           </div>
-          <div className="flex space-x-2">
-            <div className="flex w-24 items-center space-x-2">
-              <Checkbox
-                id="activeFilter"
-                checked={isActive === true}
-                onCheckedChange={(checked) => {
-                  setIsActive(checked === true)
-                }}
-              />
-              <label htmlFor="activeFilter">{"有効"}</label>
-            </div>
-            <div className="flex w-40 items-center space-x-2">
-              <Checkbox
-                id="sensitiveFilter"
-                checked={isSensitiveFilter === true}
-                onCheckedChange={(checked) =>
-                  setIsSensitiveFilter(checked === true ? true : undefined)
-                }
-              />
-              <label htmlFor="sensitiveFilter">{"センシティブ"}</label>
-            </div>
-            <div className="flex w-40 items-center space-x-2">
-              <Checkbox
-                id="ageFilter"
-                checked={isSensitiveFilter === false}
-                onCheckedChange={(checked) =>
-                  setIsSensitiveFilter(checked === true ? false : undefined)
-                }
-              />
-              <label htmlFor="ageFilter">{"全年齢"}</label>
+          <div className="flex flex-col space-x-2 space-y-2 md:flex-row">
+            <div className="flex space-x-2">
+              <div className="flex w-24 items-center space-x-2">
+                <Checkbox
+                  id="activeFilter"
+                  checked={isActive === true}
+                  onCheckedChange={(checked) => {
+                    setIsActive(checked === true)
+                  }}
+                />
+                <label htmlFor="activeFilter">{"有効"}</label>
+              </div>
+              <div className="flex w-40 items-center space-x-2">
+                <Checkbox
+                  id="sensitiveFilter"
+                  checked={isSensitiveFilter === true}
+                  onCheckedChange={(checked) =>
+                    setIsSensitiveFilter(checked === true ? true : undefined)
+                  }
+                />
+                <label htmlFor="sensitiveFilter">{"センシティブ"}</label>
+              </div>
+              <div className="flex w-40 items-center space-x-2">
+                <Checkbox
+                  id="ageFilter"
+                  checked={isSensitiveFilter === false}
+                  onCheckedChange={(checked) =>
+                    setIsSensitiveFilter(checked === true ? false : undefined)
+                  }
+                />
+                <label htmlFor="ageFilter">{"全年齢"}</label>
+              </div>
             </div>
             <Button
               variant={"secondary"}
