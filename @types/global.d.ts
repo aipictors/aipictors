c4a79099f76c21fe234a55f56d7a18912b0e4f26
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-09-15 12:02:02 +0900

    add test

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -7,6 +7,7 @@ declare global {
   const expect: typeof import("bun:test")["expect"]
   const it: typeof import("bun:test")["it"]
   const jest: typeof import("bun:test")["jest"]
+  const mock: typeof import("bun:test")["mock"]
   const afterAll: typeof import("bun:test")["afterAll"]
   const beforeAll: typeof import("bun:test")["beforeAll"]
   const afterEach: typeof import("bun:test")["afterEach"]
