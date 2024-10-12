0ad2b72f8a715ae8e6b92d8ada045519dab6d515
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-10-13 02:48:36 +0900

    リファクタ

--- app/routes/($lang)/route.tsx
+++ app/routes/($lang)/route.tsx
@@ -4,68 +4,57 @@ import { loaderClient } from "~/lib/loader-client"
 
 export const loader = async (props: LoaderFunctionArgs) => {
   const url = new URL(props.request.url)
+  const pathname = url.pathname
 
-  if (url.pathname === "/series") {
-    const url = new URL(props.request.url)
-    const user = url.searchParams.get("user")
-    const id = url.searchParams.get("id")
-
-    if (user === null || id === null) {
-      throw new Response(null, { status: 404 })
-    }
-
-    const albumResp = await loaderClient.query({
-      query: LoaderAlbumsQuery__DEPRECATED__,
-      variables: {
-        where: {
-          userId: user,
-          link: id,
-        },
-      },
-    })
-
-    if (albumResp.data.userAlbum === null) {
-      throw new Response(null, { status: 404 })
-    }
-
-    return redirect(
-      `/${albumResp.data.userAlbum.user.id}/albums/${albumResp.data.userAlbum.slug}`,
-      {
-        status: 302,
-      },
-    )
+  const redirectMapping: Record<string, string> = {
+    "/illust": "/posts/2d",
+    "/stamp-space": "/stickers",
+    "/real": "/posts/3d",
+    "/idea": "/themes",
   }
 
-  if (url.pathname === "/illust") {
-    return redirect("/posts/2d", {
-      status: 302,
-    })
+  // マッピングされたパスがあればリダイレクトを行う
+  if (redirectMapping[pathname]) {
+    return redirect(redirectMapping[pathname], { status: 302 })
   }
 
-  if (url.pathname === "/stamp-space") {
-    return redirect("/stickers", {
-      status: 302,
-    })
-  }
-
-  if (url.pathname === "/real") {
-    return redirect("/posts/3d", {
-      status: 302,
-    })
-  }
-
-  if (url.pathname === "/idea") {
-    return redirect("/themes", {
-      status: 302,
-    })
+  // "/series" の処理だけ特別に扱う
+  if (pathname === "/series") {
+    return await handleSeriesRedirect(url)
   }
 
   return null
 }
 
+const handleSeriesRedirect = async (url: URL) => {
+  const user = url.searchParams.get("user")
+  const id = url.searchParams.get("id")
+
+  if (!user || !id) {
+    throw new Response(null, { status: 404 })
+  }
+
+  const albumResp = await loaderClient.query({
+    query: LoaderAlbumsQuery__DEPRECATED__,
+    variables: {
+      where: {
+        userId: user,
+        link: id,
+      },
+    },
+  })
+
+  const album = albumResp.data.userAlbum
+  if (!album) {
+    throw new Response(null, { status: 404 })
+  }
+
+  return redirect(`/${album.user.id}/albums/${album.slug}`, { status: 302 })
+}
+
 const LoaderAlbumsQuery__DEPRECATED__ = graphql(
   `query userAlbum($where: UserAlbumWhereInput) {
-    userAlbum( where: $where) {
+    userAlbum(where: $where) {
       user {
         id
       }
