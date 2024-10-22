1bb50c8d59029bacf6c249e0df28c321319776ea
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-10-22 13:05:58 +0900

    コンポーネント整理

--- app/routes/($lang).generation._index/components/generation-config-provider.tsx
+++ app/routes/($lang).generation._index/components/generation-config-provider.tsx
@@ -3,6 +3,7 @@ import { GenerationConfigPersistent } from "~/routes/($lang).generation._index/c
 import { GenerationConfigRestoration } from "~/routes/($lang).generation._index/components/generation-config-restoration"
 import { GenerationConfigContext } from "~/routes/($lang).generation._index/contexts/generation-config-context"
 import { GenerationConfigState } from "~/routes/($lang).generation._index/machines/models/generation-config-state"
+import { getDefaultNegativePrompt } from "~/routes/($lang).generation._index/utils/get-default-negative-prompt"
 
 type Props = {
   children: React.ReactNode
@@ -13,75 +14,7 @@ type Props = {
  * @param props
  */
 export function GenerationConfigProvider(props: Props) {
-  // TODO: 後で移動
-  const getDefaultNegativePrompt = (modelType: string) => {
-    if (modelType === "SD1") {
-      return "EasyNegative"
-    }
-    if (modelType === "SD2") {
-      return "Mayng"
-    }
-    if (modelType === "SDXL") {
-      return "negativeXL_D"
-    }
-    return "EasyNegative"
-  }
-
   const modelType = config.generationFeature.defaultImageModelType
-
-  // TODO: 後で移動
-  const cacheStorage = new GenerationConfigState({
-    modelId: config.generationFeature.defaultImageModelId,
-    searchModelId: null,
-    searchModelName: null,
-    modelIds: config.generationFeature.defaultImageModelIds,
-    favoriteModelIds: [],
-    promptText: "",
-    negativePromptText: getDefaultNegativePrompt(modelType),
-    sampler: config.generationFeature.defaultSamplerValue,
-    scale: config.generationFeature.defaultScaleValue,
-    seed: -1,
-    sizeType: "SD1_512_768",
-    steps: config.generationFeature.defaultStepsValue,
-    vae: config.generationFeature.defaultVaeValue,
-    modelType: modelType,
-    clipSkip: config.generationFeature.defaultClipSkipValue,
-    isUseRecommendedPrompt:
-      config.generationFeature.defaultIsUseRecommendedPrompt,
-    i2iImageBase64: "",
-    i2iDenoisingStrengthSize:
-      config.generationFeature.defaultI2iDenoisingStrengthSize,
-    previewTaskId: null,
-    viewTaskId: null,
-    viewTaskIds: [],
-    thumbnailSizeInPromptView:
-      config.generationFeature.defaultThumbnailSizeInPromptView,
-    thumbnailSizeInHistoryListFull:
-      config.generationFeature.defaultThumbnailSizeInHistoryListFull,
-    taskListThumbnailType: config.generationFeature.defaultThumbnailType,
-    upscaleSize: 1,
-    page: 1,
-    controlNetImageBase64: "",
-    controlNetMaskImageBase64: "",
-    controlNetControlMode: "",
-    controlNetEnabled: false,
-    controlNetGuidanceEnd: 0,
-    controlNetGuidanceStart: 0,
-    controlNetPixelPerfect: false,
-    controlNetProcessorRes: 0,
-    controlNetResizeMode: "",
-    controlNetThresholdA: 0.5,
-    controlNetThresholdB: 0.5,
-    controlNetWeight: 1,
-    controlNetModule: null,
-    controlNetModel: null,
-    controlNetSaveDetectedMap: null,
-    controlNetHrOption: null,
-    previewImageURL: null,
-    currentUserToken: null,
-    generationCount: 1,
-    languageUsedForPrompt: null,
-  })
 
   const stateText =
     typeof document !== "undefined"
@@ -94,7 +27,58 @@ export function GenerationConfigProvider(props: Props) {
     <GenerationConfigContext.Provider
       options={{
         snapshot: snapshot,
-        input: cacheStorage,
+        input: new GenerationConfigState({
+          modelId: config.generationFeature.defaultImageModelId,
+          searchModelId: null,
+          searchModelName: null,
+          modelIds: config.generationFeature.defaultImageModelIds,
+          favoriteModelIds: [],
+          promptText: "",
+          negativePromptText: getDefaultNegativePrompt(modelType),
+          sampler: config.generationFeature.defaultSamplerValue,
+          scale: config.generationFeature.defaultScaleValue,
+          seed: -1,
+          sizeType: "SD1_512_768",
+          steps: config.generationFeature.defaultStepsValue,
+          vae: config.generationFeature.defaultVaeValue,
+          modelType: modelType,
+          clipSkip: config.generationFeature.defaultClipSkipValue,
+          isUseRecommendedPrompt:
+            config.generationFeature.defaultIsUseRecommendedPrompt,
+          i2iImageBase64: "",
+          i2iDenoisingStrengthSize:
+            config.generationFeature.defaultI2iDenoisingStrengthSize,
+          previewTaskId: null,
+          viewTaskId: null,
+          viewTaskIds: [],
+          thumbnailSizeInPromptView:
+            config.generationFeature.defaultThumbnailSizeInPromptView,
+          thumbnailSizeInHistoryListFull:
+            config.generationFeature.defaultThumbnailSizeInHistoryListFull,
+          taskListThumbnailType: config.generationFeature.defaultThumbnailType,
+          upscaleSize: 1,
+          page: 1,
+          controlNetImageBase64: "",
+          controlNetMaskImageBase64: "",
+          controlNetControlMode: "",
+          controlNetEnabled: false,
+          controlNetGuidanceEnd: 0,
+          controlNetGuidanceStart: 0,
+          controlNetPixelPerfect: false,
+          controlNetProcessorRes: 0,
+          controlNetResizeMode: "",
+          controlNetThresholdA: 0.5,
+          controlNetThresholdB: 0.5,
+          controlNetWeight: 1,
+          controlNetModule: null,
+          controlNetModel: null,
+          controlNetSaveDetectedMap: null,
+          controlNetHrOption: null,
+          previewImageURL: null,
+          currentUserToken: null,
+          generationCount: 1,
+          languageUsedForPrompt: null,
+        }),
       }}
     >
       <GenerationConfigPersistent>
