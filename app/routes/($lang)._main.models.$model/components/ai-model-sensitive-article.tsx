64a0a9ad49e530abb61eaa98cd9f241e00c84a72
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-10-30 16:03:44 +0900

    センシティブなモデルページのリファクタ

--- app/routes/($lang)._main.models.$model/components/ai-model-sensitive-article.tsx
+++ app/routes/($lang)._main.models.$model/components/ai-model-sensitive-article.tsx
@@ -1,25 +1,29 @@
-import { graphql, type FragmentOf } from "gql.tada"
+import { graphql, readFragment, type FragmentOf } from "gql.tada"
 import { ResponsivePagination } from "~/components/responsive-pagination"
 import { ResponsivePhotoWorksAlbum } from "~/components/responsive-photo-works-album"
+import { AiModelHeader } from "~/routes/($lang)._main.models.$model/components/ai-model-header"
 import { useContext, useEffect, useState } from "react"
 import { useQuery } from "@apollo/client/index"
 import { AuthContext } from "~/contexts/auth-context"
-import { useNavigate, useSearchParams } from "@remix-run/react"
+import { useSearchParams } from "@remix-run/react"
 import { WorkListItemFragment } from "~/routes/($lang)._main.posts._index/components/work-list"
-import { AiModelSensitiveHeader } from "~/routes/($lang)._main.models.$model/components/ai-model-sensitive-header"
+import { aiModelQuery } from "~/routes/($lang)._main.models.$model/route"
 
 type Props = {
-  name: string
-  thumbnailImageURL: string | null
-  works: FragmentOf<typeof WorkListItemFragment>[]
-  worksCount: number
+  model: FragmentOf<typeof ModelSensitiveItemFragment>
   isMoreRatings: boolean
   hasPrompt: boolean
   page: number
 }
 
 export function AiModelSensitiveArticle(props: Props) {
-  if (props.works === null || props.works.length === 0) {
+  const model = readFragment(ModelSensitiveItemFragment, props.model)
+
+  if (
+    model.works === undefined ||
+    model.works === null ||
+    model.worksCount === 0
+  ) {
     return (
       <div className="text-center">
         <p>作品がありません</p>
@@ -31,15 +35,17 @@ export function AiModelSensitiveArticle(props: Props) {
 
   const [page, setPage] = useState(props.page)
 
-  const { data } = useQuery(aiModelWorksQuery, {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const { data } = useQuery(aiModelQuery, {
     skip: authContext.isLoading || authContext.isNotLoggedIn,
     variables: {
-      search: props.name,
+      search: model.name,
       offset: 32 * page,
       limit: 32,
       where: {
         ratings: props.isMoreRatings ? ["R18", "R18G"] : ["R18"],
-        isSensitive: true,
+        isSensitive: false,
         hasPrompt: props.hasPrompt,
         ...(props.hasPrompt && {
           isPromptPublic: true,
@@ -49,35 +55,29 @@ export function AiModelSensitiveArticle(props: Props) {
     fetchPolicy: "cache-first",
   })
 
-  const works = data?.aiModel?.works ?? props.works
+  const aiModel = readFragment(ModelSensitiveItemFragment, data?.aiModel)
 
-  const navigate = useNavigate()
-
-  const [searchParams, setSearchParams] = useSearchParams()
+  const works = aiModel?.works ?? model.works
 
   // URLパラメータの監視と更新
   useEffect(() => {
-    const params = new URLSearchParams(searchParams)
-    params.set("page", String(page))
-    params.set("prompt", props.hasPrompt ? "1" : "0")
-    params.set("r18g", props.isMoreRatings ? "1" : "0")
-    navigate(`?${params.toString()}`, { replace: true })
-  }, [page, props.hasPrompt, props.isMoreRatings])
+    setSearchParams({
+      page: String(page),
+      prompt: props.hasPrompt ? "1" : "0",
+      r15: props.isMoreRatings ? "1" : "0",
+    })
+  }, [page, props.hasPrompt, props.isMoreRatings, setSearchParams])
 
   return (
     <div className="flex flex-col space-y-4">
-      <AiModelSensitiveHeader
-        name={props.name}
+      <AiModelHeader
+        name={model.name}
         thumbnailImageURL={
-          props.thumbnailImageURL
-            ? props.thumbnailImageURL
-            : works.length
-              ? works[0].largeThumbnailImageURL
-              : null
+          works.length ? works[0].largeThumbnailImageURL : null
         }
         isMoreRatings={props.isMoreRatings}
         hasPrompt={props.hasPrompt}
-        worksCount={props.worksCount}
+        worksCount={model.worksCount}
       />
       <div className="space-y-4">
         <ResponsivePhotoWorksAlbum
@@ -89,12 +89,9 @@ export function AiModelSensitiveArticle(props: Props) {
         <div className="-translate-x-1/2 fixed bottom-0 left-1/2 z-10 w-full border-border/40 bg-background/95 p-2 backdrop-blur supports-[backdrop-filter]:bg-background/80">
           <ResponsivePagination
             perPage={16}
-            maxCount={props.worksCount}
+            maxCount={model.worksCount}
             currentPage={page}
-            onPageChange={(page: number) => {
-              setPage(page)
-              navigate(`?page=${page}`)
-            }}
+            onPageChange={(page: number) => setPage(page)}
           />
         </div>
       </div>
@@ -102,13 +99,18 @@ export function AiModelSensitiveArticle(props: Props) {
   )
 }
 
-const aiModelWorksQuery = graphql(
-  `query AiModel($search: String!, $limit: Int!, $offset: Int!, $where: WorksWhereInput) {
-    aiModel(where: {search: $search}) {
-      works(limit: $limit, offset: $offset, where: $where) {
+export const ModelSensitiveItemFragment = graphql(
+  `fragment ModelItem on AiModelNode {
+      id
+      name
+      type
+      worksCount
+      generationModelId
+      workModelId
+      thumbnailImageURL
+      works(offset: $offset, limit: $limit, where: $where) {
         ...WorkListItem
       }
-    }
   }`,
   [WorkListItemFragment],
 )
