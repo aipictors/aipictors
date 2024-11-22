ebd2bf22a119b5016389dcfdcefe1a5531374bda
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-11-22 12:51:06 +0900

    Update line-logged-in-with-url-button.tsx

--- app/components/line-logged-in-with-url-button.tsx
+++ app/components/line-logged-in-with-url-button.tsx
@@ -1,5 +1,6 @@
-import { useSuspenseQuery } from "@apollo/client/index"
+import { useQuery } from "@apollo/client/index"
 import { graphql } from "gql.tada"
+import { toast } from "sonner"
 import { LineLoggedInButton } from "~/components/button/line-logged-in-button"
 
 type Props = {
@@ -10,22 +11,24 @@ type Props = {
  * LINEログインボタン
  */
 export function LineLoggedInWithUrlButton(props: Props) {
-  const { data } = useSuspenseQuery(authenticationLineAccountUrlQuery, {})
+  const { data } = useQuery(authenticationLineAccountUrlQuery, {})
 
-  const url = data.authenticationLineAccountUrl
+  const url = data?.authenticationLineAccountUrl
 
   const onClick = () => {
     if (url === null || url === "") {
       return
     }
-    window.location.href = url
+    window.location.href = url ?? ""
   }
 
-  if (url === null || url === "") {
+  if (url === undefined || url === null || url === "") {
     return (
       <LineLoggedInButton
         text={props.text}
-        onClick={() => {}}
+        onClick={() => {
+          toast("LINEログインのURLが取得できませんでした。")
+        }}
         disabled={true}
       />
     )
