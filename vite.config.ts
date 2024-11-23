df399a5661758c4517b27de07046be8b7fbc08e8
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-11-23 13:20:38 +0900

    adopt v3 future flags

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { vitePlugin, cloudflareDevProxyVitePlugin } from "@remix-run/dev"
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin,
+} from "@remix-run/dev"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { viteStaticCopy } from "vite-plugin-static-copy"
+
+declare module "@remix-run/cloudflare" {
+  interface Future {
+    v3_singleFetch: true
+  }
+}
 
 export default defineConfig({
   worker: {
@@ -12,7 +21,17 @@ export default defineConfig({
   },
   plugins: [
     cloudflareDevProxyVitePlugin(),
-    vitePlugin({}),
+    remix({
+      future: {
+        unstable_optimizeDeps: true,
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+        v3_routeConfig: true,
+      },
+    }),
     tsconfigPaths(),
     viteStaticCopy({
       targets: [
