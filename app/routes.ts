eacb164f53645c2c9c9fd7776d60746e7d89382a
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-11-23 14:09:02 +0900

    fix route.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
 import { flatRoutes } from "@remix-run/fs-routes"
 import type { RouteConfig } from "@remix-run/route-config"
 
-export const routes: RouteConfig = flatRoutes()
+export default flatRoutes() satisfies RouteConfig
