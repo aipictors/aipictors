797512505b5c67d2063c375753bb269576895057
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2024-11-25 16:03:52 +0900

    メンテナンス中を解除する

--- app/components/app/app-header.tsx
+++ app/components/app/app-header.tsx
@@ -6,15 +6,13 @@ type Props = Readonly<{
 export function AppHeader(props: Props) {
   return (
     <>
-      <header className="fixed z-50 h-full w-full">
+      <header className="fixed z-50 w-full">
         <div
           className={
-            "flex h-full w-full max-w-none items-center justify-between gap-x-4 border-border/40 bg-white md:px-8"
+            "flex w-full max-w-none items-center justify-between gap-x-4 border-border/40 bg-background/95 px-4 py-4 backdrop-blur supports-[backdrop-filter]:bg-background/80 md:px-8"
           }
         >
-          <div className="m-auto text-black">
-            {"ただいまメンテナンス中です"}
-          </div>
+          {props.children}
         </div>
         {props.announcement && props.announcement}
       </header>
