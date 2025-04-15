56df4fc939a1d45d6bb2fe3d70353358b04c8c78
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2025-04-16 04:45:52 +0900

    feat: デザイン調整

--- app/routes/($lang)._main._index/components/home-banners.tsx
+++ app/routes/($lang)._main._index/components/home-banners.tsx
@@ -24,7 +24,7 @@ export function HomeBanners(props: Props) {
   return (
     <Carousel opts={{ dragFree: true, loop: true }}>
       <CarouselContent className="m-auto">
-        <CarouselItem className="basis-1/3 pl-0 xl:basis-1/3">
+        <CarouselItem className="basis-1/1 pl-0 xl:basis-1/1">
           <Link to="https://www.aipictors.com/events/spring">
             <img
               alt="home_banner_01"
@@ -33,7 +33,7 @@ export function HomeBanners(props: Props) {
             />
           </Link>
         </CarouselItem>
-        <CarouselItem className="basis-1/3 xl:basis-1/3">
+        <CarouselItem className="basis-1/1 xl:basis-1/1">
           <Link to="/generation">
             <img
               alt="home_banner_01"
@@ -42,7 +42,37 @@ export function HomeBanners(props: Props) {
             />
           </Link>
         </CarouselItem>
-        <CarouselItem className="basis-1/3 xl:basis-1/3">
+        <CarouselItem className="basis-1/1 xl:basis-1/1">
+          <Link
+            target="_blank"
+            to="https://docs.google.com/forms/d/e/1FAIpQLSfyDAMllfLp8PyKJFEFhm8K7bQnSm0Nc066opKcoSp130_gkg/viewform?usp=pp_url"
+          >
+            <img
+              alt="home_banner_01"
+              className="h-40 w-auto md:h-48 md:max-h-auto"
+              src="https://assets.aipictors.com/home_banner_03.webp"
+            />
+          </Link>
+        </CarouselItem>
+        <CarouselItem className="basis-1/1 pl-0 xl:basis-1/1">
+          <Link to="https://www.aipictors.com/events/spring">
+            <img
+              alt="home_banner_01"
+              className="h-40 w-auto md:h-48 md:max-h-auto"
+              src="https://assets.aipictors.com/spring-event.webp"
+            />
+          </Link>
+        </CarouselItem>
+        <CarouselItem className="basis-1/1 xl:basis-1/1">
+          <Link to="/generation">
+            <img
+              alt="home_banner_01"
+              className="h-40 w-auto md:h-48 md:max-h-auto"
+              src="https://assets.aipictors.com/home_banner_02.webp"
+            />
+          </Link>
+        </CarouselItem>
+        <CarouselItem className="basis-1/1 xl:basis-1/1">
           <Link
             target="_blank"
             to="https://docs.google.com/forms/d/e/1FAIpQLSfyDAMllfLp8PyKJFEFhm8K7bQnSm0Nc066opKcoSp130_gkg/viewform?usp=pp_url"
