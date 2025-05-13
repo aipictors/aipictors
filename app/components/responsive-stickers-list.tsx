8f3b536af2a115f9f439b0a5fef33140424b885c
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2025-05-14 08:23:10 +0900

    feat: スタンプ一覧のコンポーネントにて@_unmaskを廃止する

--- app/components/responsive-stickers-list.tsx
+++ app/components/responsive-stickers-list.tsx
@@ -1,57 +1,60 @@
 import { Link } from "@remix-run/react"
-import { graphql, type FragmentOf } from "gql.tada"
+import { graphql, readFragment, type FragmentOf } from "gql.tada"
 import { Download, MessageCircle } from "lucide-react"
 
 type Props = {
   stickers: FragmentOf<typeof StickerListItemFragment>[]
-  targetRowHeight?: number
 }
 
 /**
  * レスポンシブ対応のスタンプ一覧
  */
 export function ResponsiveStickersList(props: Props) {
-  if (props.stickers === null || props.stickers.length === 0) {
+  if (!props.stickers || props.stickers.length === 0) {
     return null
   }
 
   return (
     <div className="flex flex-wrap">
-      {props.stickers.map((sticker) => (
-        <div key={sticker.id} className="m-2 overflow-hidden rounded-md">
-          <Link
-            to={`/stickers/${sticker.id}`}
-            className="group relative block p-1"
-          >
-            <div className="overflow-hidden rounded-md">
-              <img
-                className="m-auto max-w-32 rounded-md transition-transform duration-200 ease-in-out group-hover:scale-105"
-                src={sticker.imageUrl ?? ""}
-                alt={sticker.title}
-              />
-            </div>
-            <div className="h-24 max-w-32 space-y-2 overflow-hidden rounded-md">
-              <span className="text-ellipsis text-nowrap text-sm">
-                {sticker.title}
-              </span>
-              <div className="flex items-center">
-                <Download size={16} />
-                <span>{sticker.downloadsCount}</span>
+      {props.stickers.map((stickerRef) => {
+        const sticker = readFragment(StickerListItemFragment, stickerRef)
+
+        return (
+          <div key={sticker.id} className="m-2 overflow-hidden rounded-md">
+            <Link
+              to={`/stickers/${sticker.id}`}
+              className="group relative block p-1"
+            >
+              <div className="overflow-hidden rounded-md">
+                <img
+                  className="m-auto max-w-32 rounded-md transition-transform duration-200 ease-in-out group-hover:scale-105"
+                  src={sticker.imageUrl ?? ""}
+                  alt={sticker.title}
+                />
               </div>
-              <div className="flex items-center">
-                <MessageCircle size={16} />
-                <span>{sticker.usesCount}</span>
+              <div className="h-24 max-w-32 space-y-2 overflow-hidden rounded-md">
+                <span className="text-ellipsis whitespace-nowrap text-sm">
+                  {sticker.title}
+                </span>
+                <div className="flex items-center">
+                  <Download size={16} />
+                  <span>{sticker.downloadsCount.toLocaleString()}</span>
+                </div>
+                <div className="flex items-center">
+                  <MessageCircle size={16} />
+                  <span>{sticker.usesCount.toLocaleString()}</span>
+                </div>
               </div>
-            </div>
-          </Link>
-        </div>
-      ))}
+            </Link>
+          </div>
+        )
+      })}
     </div>
   )
 }
 
 export const StickerListItemFragment = graphql(
-  `fragment StickerListItem on StickerNode @_unmask {
+  `fragment StickerListItem on StickerNode {
     id
     title
     downloadsCount
