d5184924b74294c061412fe91eb707971beeb634
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2025-07-27 22:25:37 +0900

    Update user-tabs.tsx

--- app/routes/($lang)._main.users.$user._index/components/user-tabs.tsx
+++ app/routes/($lang)._main.users.$user._index/components/user-tabs.tsx
@@ -5,6 +5,7 @@ import { useLocale } from "~/hooks/use-locale"
 import { useUserActiveTab } from "~/routes/($lang)._main.users.$user._index/hooks/use-user-active-tab"
 import { useUserTabLabels } from "~/routes/($lang)._main.users.$user._index/hooks/use-user-tab-label"
 import { handleUserTabNavigation } from "~/routes/($lang)._main.users.$user._index/utils/handle-user-tab-navigation"
+import { useQuery } from "@apollo/client/index"
 
 type Props = {
   user: FragmentOf<typeof UserTabsFragment>
@@ -17,16 +18,29 @@ export function UserTabs(props: Props) {
 
   const locale = useLocale()
 
+  // 実際のユーザー作品数を確認するクエリ
+  const { data: worksCountData } = useQuery(UserWorksCountQuery, {
+    variables: {
+      userId: user.id,
+    },
+  })
+
   const getActiveTab = useUserActiveTab({
     url: location.pathname,
     lang: locale,
   })
 
+  // 実際の作品数でタブの表示判定を行う
+  const actualImageWorksCount = worksCountData?.imageWorksCount ?? 0
+  const actualNovelWorksCount = worksCountData?.novelWorksCount ?? 0
+  const actualVideoWorksCount = worksCountData?.videoWorksCount ?? 0
+  const actualColumnWorksCount = worksCountData?.columnWorksCount ?? 0
+
   const tabLabels = useUserTabLabels({
-    hasImageWorks: user.hasImageWorks,
-    hasNovelWorks: user.hasNovelWorks,
-    hasVideoWorks: user.hasVideoWorks,
-    hasColumnWorks: user.hasColumnWorks,
+    hasImageWorks: user.hasImageWorks || actualImageWorksCount > 0,
+    hasNovelWorks: user.hasNovelWorks || actualNovelWorksCount > 0,
+    hasVideoWorks: user.hasVideoWorks || actualVideoWorksCount > 0,
+    hasColumnWorks: user.hasColumnWorks || actualColumnWorksCount > 0,
     hasFolders: user.hasFolders,
     hasAlbums: user.hasAlbums,
     hasPublicStickers: user.hasPublicStickers,
@@ -75,5 +89,14 @@ export const UserTabsFragment = graphql(`
     hasAlbums
     hasPublicStickers
     hasBadges
+  }
+`)
+
+export const UserWorksCountQuery = graphql(`
+  query UserWorksCount($userId: ID!) {
+    imageWorksCount: worksCount(where: { userId: $userId, workType: WORK, ratings: [G, R15] })
+    novelWorksCount: worksCount(where: { userId: $userId, workType: NOVEL, ratings: [G, R15] })
+    videoWorksCount: worksCount(where: { userId: $userId, workType: VIDEO, ratings: [G, R15] })
+    columnWorksCount: worksCount(where: { userId: $userId, workType: COLUMN, ratings: [G, R15] })
   }
 `)
