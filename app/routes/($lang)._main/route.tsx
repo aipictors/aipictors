7928d2bce91e6bca16d08cc4818cce2f8337c61d
N.K. <124619691+nakatsuka-k@users.noreply.github.com>
2026-04-13 21:39:33 +0900

    管理画面のレイアウト修正

--- app/routes/($lang)._main/route.tsx
+++ app/routes/($lang)._main/route.tsx
@@ -1,12 +1,21 @@
 import { AppCommonLayout } from "~/components/app/app-common-layout"
 import { AppLoadingPage } from "~/components/app/app-loading-page"
-import { Outlet } from "@remix-run/react"
+import { Outlet, useLocation } from "@remix-run/react"
 
 export function HydrateFallback () {
   return <AppLoadingPage />
 }
 
 export default function MainLayout () {
+  const location = useLocation()
+  const isAdminRoute = /^\/(ja|en)\/admin(\/|$)|^\/admin(\/|$)/.test(
+    location.pathname,
+  )
+
+  if (isAdminRoute) {
+    return <Outlet />
+  }
+
   return (
     <>
       <AppCommonLayout outlet={<Outlet />} />
